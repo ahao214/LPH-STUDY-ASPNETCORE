57678b41e93336849d1798f16473606f6002e6b7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-19 09:01:01 +0800

    顶级语句

---
+++ 02_顶级语句/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System.Reflection;
+
+/*
+ 顶级语句
+ */
+
+//获取当前代码所在方法名和类名
+
+WriteLine(MethodBase.GetCurrentMethod().Name);
+WriteLine(MethodBase.GetCurrentMethod().DeclaringType.Name);
+
+
+
+Test();
+
+void Test()
+{
+    WriteLine("good good study,day day up");
+}
