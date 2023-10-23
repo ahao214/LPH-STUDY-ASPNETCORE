1083c0bb9c040e26f40c40f9bf1792cc72cb9e2c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-23 22:44:29 +0800

    方法的调用

---
+++ 方法的调用/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+Title = "方法的调用";
+
+
+WriteLine("开始调用方法");
+MethodA(3, 4);
+WriteLine("结束方法调用");
+
+
+
+static void MethodA(int a, int b)
+{
+    WriteLine($"MehtoA:a={a},b={b}");
+    MethodB(1, 2);
+    WriteLine("退出方法MethodA");
+}
+
+
+static void MethodB(int a, int b)
+{
+    WriteLine($"MehtoB:a={a},b={b}");
+    WriteLine("退出方法MethodB");
+}
