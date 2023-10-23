0bcb728c5f5f279a183965eca456ebcc031420b3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-23 22:38:50 +0800

    命名参数和可选参数

---
+++ 命名参数和可选参数/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+Title = "命名参数和可选参数";
+
+/*
+注意：
+命名参数和可选参数在一起的时候
+命名参数在前，可选参数在后
+ */
+
+Print(1, "jack");
+//命名参数
+Print(s: "Tom", a: 100);
+
+static void Print(int a, string s)
+{
+
+}
+
+
+
+
+
+
+
+//可选参数
+WriteWord(1, "hello");
+WriteWord(1);
+static void WriteWord(int a ,string s="")
+{
+
+}
