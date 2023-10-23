42d80cd1eee8a354354103935379f56798f8b3d1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-23 22:23:01 +0800

    输出参数Out参数

---
+++ Out/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+Title = "输出参数Out参数";
+
+
+int a = 1;
+WriteLine(a);
+int res = Add(out a);
+WriteLine($"调用Add方法返回的结果:{res}");
+WriteLine($"Add方法中OutValue的值:{a}");
+
+
+static int Add(out int outValue)
+{
+    outValue = 5;
+    int returnValue = outValue + 1;
+    return returnValue;
+}
