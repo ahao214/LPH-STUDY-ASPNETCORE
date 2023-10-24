e4be68a1e27035972fa67422531eea983bb8ad1c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-24 20:41:07 +0800

    IdentityServer

---
+++ Sample.IdentityServer/Config.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer4.Models;
+
+
+
+namespace Sample.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes = new[]
+        {
+            new ApiScope
+            {
+                Name ="sample_api",
+                DisplayName="Sample API"
+            },
+        }
+
+
+        public static IEnumerable<Client> Clients = new[]
+        {
+            new Client
+            {
+                ClientId="sample_client",
+                ClientSecrets =
+                {
+                    new Secret("sample_client_secret".Sha256())
+                },
+                AllowedGrantTypes= GrantTypes.ClientCredentials,
+                AllowedScopes={"sample_api"}
+            },
+        }
+
+    }
+}
