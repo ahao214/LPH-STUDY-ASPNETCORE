c40148c13c311afefbc86a3094af8304d0a2d0dc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-25 22:45:13 +0800

    递归

--- 递归/Program.cs
+++ 递归/Program.cs
@@ -1,2 +1,23 @@
 ﻿using static System.Console;
 
+/*
+递归 
+*/
+
+Recursion(5);
+
+
+static void Recursion(int a)
+{
+    Recursion(a);
+    if (a == 0)
+    {
+        return;
+    }
+    else
+    {
+        Recursion(a - 1);
+    }
+
+    WriteLine($"a={a}");
+}
