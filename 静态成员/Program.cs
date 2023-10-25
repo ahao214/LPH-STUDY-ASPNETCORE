d3639f3bbc9a717a8a49e14e176ca1319d1346a8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-25 23:02:38 +0800

    静态成员的分类

---
+++ 静态成员/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Title = "静态成员的分类";
+
+
+/*
+ 初始静态成员可以使用静态构造方法
+ */
+
+Student.Member = 30;
+WriteLine(Student.PI);
+
+
+class Student
+{
+    public const double PI = 3.14;
+    public static int Member;
+
+
+}
