97996d4d37375593430ccd65c3f3a87a4729defd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-25 21:44:42 +0800

    最小活跃数负载均衡算法

---
+++ LoadBalancer/LoadBalancers/LeastActiveLoadBalancer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace LoadBalancer.LoadBalancers
+{
+    /// <summary>
+    /// 最小活跃数负载均衡算法
+    /// </summary>
+    public class LeastActiveLoadBalancer
+    {
+        //1、实例地址集合（集群）
+        private List<string> serverAddresss = new List<string>();
+
+        public LeastActiveLoadBalancer()
+        {
+            serverAddresss.Add("http://localhost5001");
+            serverAddresss.Add("http://localhost5002");
+            serverAddresss.Add("http://localhost5003");
+        }
+
+        /// <summary>
+        /// 最小活跃数选择服务地址
+        /// </summary>
+        /// <returns></returns>
+        public string Select()
+        {
+            return null;
+        }
+
+
+    }
+}
