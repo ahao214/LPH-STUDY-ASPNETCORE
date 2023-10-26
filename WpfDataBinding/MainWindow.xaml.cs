4a15261404541e0a7f5bad8e709138c71f471d03
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 15:37:43 +0800

    wpf数据基础绑定

---
+++ WpfDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDataBinding.Models;
+
+namespace WpfDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// 定义Age属性
+        /// </summary>
+        public int Age { set; get; } = 100;
+
+        public Student? Stu { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 创建学生对象
+            Stu = new Student()
+            {
+                Id = 1001,
+                Name = "Jack",
+                Books= new List<string>
+                {
+                    "a","b","c"
+                }
+            };
+
+            // 可以指定窗体绑定的数据源
+            //this.DataContext = Stu;
+            this.DataContext = this;
+        }
+    }
+}
---
+++ WpfDataBinding/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataBinding.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<string> Books { get; set; } = new List<string>(0);
+    }
+}
