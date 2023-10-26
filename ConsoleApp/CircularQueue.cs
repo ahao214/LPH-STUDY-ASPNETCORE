98084f9a9523e4e83850a220004d041465830c6c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 12:49:41 +0800

    循环队列

--- ConsoleApp/CircularQueue.cs
+++ ConsoleApp/CircularQueue.cs
@@ -6,9 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    public class CircularQueue
+    public class CircularQueue<T>
     {
+        // 循环队列
+        private readonly T[] _items;
+        private readonly int _size;
+        private int _head;
+        private int _tail;
+
+        public CircularQueue(int size)
+        {
+            _items = new T[size];
+            _size = size;
+        }
 
 
+        public bool Enqueue(T item)
+        {
+            // 队满的条件
+            if ((_tail + 1) % _size == _head)
+                return false;
+            _items[_tail] = item;
+            _tail = (_tail + 1) % _size;
+            return true;
+        }
+
+        public T Dequeue()
+        {
+            if (_head == _tail)
+                return default;
+            var item = _items[_head];
+            _head = (_head + 1) % _size;
+            return item;
+        }
     }
 }
