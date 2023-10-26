bf106dfbd7088d24f24a1014b33eb16efd8c48f7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 19:50:36 +0800

    分部类

---
+++ 分部类/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+Title = "分部类";
+
+/*
+优点：
+代码组织
+代码复用
+模块化开发
+大型项目开发
+
+某个文件特别大的时候,可以使用分部类
+
+
+
+
+*/
+
+
+
+
+
+partial class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+partial class Student
+{
+    public void ShowInfo()
+    {
+        WriteLine(Id);
+        WriteLine(Name);
+    }
+}
