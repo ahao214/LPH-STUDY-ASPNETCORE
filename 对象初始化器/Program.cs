8ce940e16fd8cfd7719302c5ba642842c8be8d7f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 16:01:02 +0800

    静态构造方法的使用

---
+++ 对象初始化器/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+Title = "对象初始化器";
+
+//对象初始化
+Student stu = new Student()
+{
+    Id = 1,
+    Name = "Jack"
+};
+
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
