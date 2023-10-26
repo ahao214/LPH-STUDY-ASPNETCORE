8e6d3fe403b1fe6f6ec9800f872fc38f1191e73a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 19:30:41 +0800

    Readonly

---
+++ ReadonlyDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+Title = "Readonly只读修饰符";
+
+
+
+
+
+class Shape
+{
+    readonly double PI = 3.1415;
+    readonly int NumberOfSides;
+
+    public Shape(double side1, double side2)
+    {
+        //4条边表示矩形
+        NumberOfSides = 4;
+    }
+
+    public Shape(double side1, double side2, double side3)
+    {
+        //3条边表示三角形
+        NumberOfSides = 3;
+    }
+
+}
