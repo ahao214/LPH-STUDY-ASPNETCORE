be009eda7b00d0149f981093821e2fd055a22c1a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 12:35:50 +0800

    队列

---
+++ ConsoleApp/ArrayQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ArrayQueue<T>
+    {
+        // 队列数组
+        private readonly T[] _items;
+        private readonly int _size = 0;
+        private int _head = 0;
+        private int _tail = 0;
+
+        public ArrayQueue(int size)
+        {
+            _items = new T[size];
+            _size = size;
+        }
+
+        public bool Enqueue(T item)
+        {
+            if (_tail == _size)
+                return false;
+            _items[_tail++] = item;
+            return true;
+        }
+
+        public T Dequeue()
+        {
+            if (_head == _tail)
+                return default;
+            var res = _items[_head];
+            return res;
+        }
+
+    }
+}
