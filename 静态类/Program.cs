fa56c6435521c64bfe4fe87296d8f550b7a6c5e9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-27 15:03:52 +0800

    静态类

---
+++ 静态类/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+Title = "静态类";
+
+
+/*
+作用：用于存放不受实例数据影响数据和函数
+1、使用static修饰符声明
+2、所有成员必须是静态的
+3、只能有一个静态构造函数
+4、隐式密封的，不能被继承
+ */
+
+
+static class Student
+{
+    static Student()
+    {
+        Id = 1;
+        Name = "Jack";
+    }
+
+    static private int Id;
+    static public string Name { get; set; }
+
+    static public void ShowInfo()
+    {
+        WriteLine($"id={id},Name={Name}");
+
+    }
+}
