e933e8f935c89c5f643150a26a0a06098efb4a4e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-27 14:17:42 +0800

    base

---
+++ Base/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+Title = "调用基类和自己的构造方法";
+
+
+
+
+
+//base 关键字作用一
+
+class Student
+{
+    public void Speak()
+    {
+        WriteLine("说中文");
+    }
+}
+
+
+class StuJack : Student
+{
+    public StuJack()
+    {
+        // 访问基类成员
+        base.Speak();
+    }
+
+    public void Speak()
+    {
+        WriteLine("说英文");
+    }
+}
+
