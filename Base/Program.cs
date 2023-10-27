84dca056260a67a943067797d38fb1e587177895
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-27 14:20:09 +0800

    base

--- Base/Program.cs
+++ Base/Program.cs
@@ -7,6 +7,7 @@ Title = "调用基类和自己的构造方法";
 
 
 //base 关键字作用一
+//访问基类成员
 
 class Student
 {
@@ -31,3 +32,30 @@ class StuJack : Student
     }
 }
 
+
+
+
+//base 关键字作用二
+//调用基类构造方法
+class Teacher
+{
+    public Teacher()
+    {
+
+    }
+
+    public Teacher(string info)
+    {
+        WriteLine(info);
+    }
+}
+
+
+class TeacherTom : Teacher
+{
+    // 调用基类构造方法
+    public TeacherTom() : base("Tom老师")
+    {
+
+    }
+}
