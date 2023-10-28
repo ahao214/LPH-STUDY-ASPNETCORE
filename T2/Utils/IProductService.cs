52c976aecbf92c77480f3493f4eac4ac77b9766e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-28 16:35:46 +0800

    添加接口

---
+++ T2/Utils/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using T2.Models;
+
+namespace T2.Utils
+{
+    interface IProductService
+    {
+        IEnumerable<Goods> GetAllGoods();
+
+    }
+}
---
+++ T2/Utils/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using T2.Models;
+
+namespace T2.Utils
+{
+    public class ProductService : IProductService
+    {
+        public IEnumerable<Goods> GetAllGoods()
+        {
+            return new List<Goods>()
+            {
+               new Goods(1,"苹果",10),
+               new Goods(2,"凤梨",12),
+               new Goods(3,"橘子",23),
+               new Goods(4,"香蕉",4),
+
+            };
+        }
+    }
+}
