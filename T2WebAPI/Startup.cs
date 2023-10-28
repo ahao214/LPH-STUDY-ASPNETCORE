245cfaeed90a83319f5dda23ac877858902c18d2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-28 15:54:12 +0800

    跨域策略

--- T2WebAPI/Startup.cs
+++ T2WebAPI/Startup.cs
@@ -22,9 +22,23 @@ namespace T2WebAPI
 
         public IConfiguration Configuration { get; }
 
+        // 跨域策略名
+        private static readonly string cors = "cors";
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // 1.添加跨域策略
+            services.AddCors(a =>
+            {
+                a.AddPolicy(cors, builder =>
+                {
+                    builder.AllowAnyOrigin();
+                });
+            });
+
+
             services.AddControllers();
         }
 
@@ -41,10 +55,12 @@ namespace T2WebAPI
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors();  // 2.使用跨域策略
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                // 3.完成跨域配置
+
+                endpoints.MapControllers().RequireCors(cors);
             });
         }
     }
