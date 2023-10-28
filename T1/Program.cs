bc1baf0ed626586680c0a07b7081c1d3f5bfad91
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-28 14:56:36 +0800

    异步方法

---
+++ T1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BoilWater();
+            Clean();
+            Pao();
+
+        }
+
+
+        static void BoilWater()
+        {
+            Console.WriteLine("烧开水");
+            Thread.Sleep(5000);
+        }
+
+        // 异步方法
+        async static void CleanAsync()
+        {
+            // 新开线程
+            string result = await CleanA();
+            Console.WriteLine(result);
+        }
+
+        // await调用的方法返回值必须为Task<T>
+        static Task<string> CleanA()
+        {
+            
+            return Task.Run<string>(() =>
+            {
+                // 匿名函数方法体 实际新开线程的执行代码
+                Thread.Sleep(5000);
+                return "清洗茶具";
+            });
+        }
+
+
+        static void Clean()
+        {
+            Console.WriteLine("清洗茶具");
+            Thread.Sleep(5000);
+        }
+
+        static void Pao()
+        {
+            Console.WriteLine("泡茶");
+            Thread.Sleep(5000);
+        }
+
+
+    }
+}
