63bb33899f3160f9781c5eda77672135b0628fc5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-28 16:53:02 +0800

    扩展方法

---
+++ 扩展方法/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+Title = "扩展方法";
+
+
+// 使用扩展方法
+Student stu = new Student();
+stu.PrintId();
+
+
+// 定义扩展方法
+class Student
+{
+    public int Id { get; set; }
+}
+
+static class Student2
+{
+    public static void PrintId(this Student student)
+    {
+        WriteLine(student.Id);
+    }
+}
+
