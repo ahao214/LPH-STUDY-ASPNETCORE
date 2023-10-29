0d6ffb1577810b16bdd2ecd61e2c03149e7b1e60
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 18:57:09 +0800

    统计方法

--- 05_统计方法/Program.cs
+++ 05_统计方法/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using _05_统计方法;
+using static System.Console;
 
 Title = "统计方法";
 
@@ -8,7 +9,28 @@ int[] arr = { 2, 3, 3, 3, 3, 31, 2, 345, 45, 34, 78, 123, 45, 67, 90, 8 };
 WriteLine($"和：{arr.Sum()},平均数：{arr.Average():0.00},数量：{arr.Count()},最大值：{arr.Max()},最小值：{arr.Min()}");
 
 
+List<Student> students = new List<Student>()
+{
+new Student{Id=1,Name ="包青天",Age = 22,Sex ="男",English =100},
 
+new Student{Id=2,Name ="展昭",Age = 34,Sex ="男",English =98},
+
+new Student{Id=1,Name ="张龙",Age = 23,Sex ="男",English =77},
+
+new Student{Id=1,Name ="赵虎",Age = 25,Sex ="男",English =45},
+
+new Student{Id=1,Name ="王超",Age = 27,Sex ="男",English =67},
+new Student{Id=1,Name ="马汉",Age = 30,Sex ="男",English =78},
+
+new Student{Id=1,Name ="苏小妹",Age = 18,Sex ="女",English =100},
+
+
+
+
+};
+
+
+ReadKey();
 
 
 
---
+++ 05_统计方法/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_统计方法
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public int English { get; set; }
+
+    }
+}
