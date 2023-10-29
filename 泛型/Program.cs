ec4686a7a86598a612a8928df497caf1f3d101e3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 14:07:31 +0800

    多个参数的泛型类型

--- 泛型/Program.cs
+++ 泛型/Program.cs
@@ -2,7 +2,10 @@
 
 Title = "泛型";
 
-
+/*
+ * 泛型
+ * 一种通用类型，它可以定义没有明确类型的成员
+ */
 
 Student<string> s1 = new Student<string>();
 s1.Id = "1";
@@ -11,12 +14,14 @@ Student<int> s2 = new Student<int>();
 s2.Id = 1;
 
 
+//多个参数的泛型
+Teacher<int, string, int> teacher = new Teacher<int, string, int>();
 
 
 
 class Student1
 {
-    public int Id { get; set; } 
+    public int Id { get; set; }
 }
 
 
@@ -30,3 +35,17 @@ class Student<T>
     public T Id { get; set; }
 }
 
+
+
+//多个参数的泛型类型
+class Teacher<T, S, V>
+{
+    public T Id { get; set; }
+    public S Phone { get; set; }
+    public S Name { get; set; }
+    public V ShowInfo(T id)
+    {
+        return default;
+    }
+
+}
