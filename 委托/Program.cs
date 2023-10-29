7b15f660ee0ba103a6ff019550782ee1c4cb88a7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 13:54:05 +0800

    委托的好处

--- 委托/Program.cs
+++ 委托/Program.cs
@@ -3,19 +3,37 @@
 Title = "委托";
 
 /*
- 
+ 委托的好处
+灵活、可重用、可维护
  */
 
 
 Calulate del = new Calulate(Add);
 WriteLine(del(1, 2));
 
+MyClass.Call(Sub);
+
 static int Add(int a, int b)
 {
     return a + b;
 }
 
+static int Sub(int a, int b)
+{
+    return a - b;
+}
+
+
+class MyClass
+{
+    public static void Call(Calulate del)
+    {
+        WriteLine(del(1, 2));
+    }
+}
+
 public delegate int Calulate(int x, int y);
+
 
 
 
