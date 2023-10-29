7d18f3efbbfd30f27cfa40279cedec6f0329719a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 13:50:00 +0800

    委托

--- 委托/Program.cs
+++ 委托/Program.cs
@@ -2,3 +2,22 @@
 
 Title = "委托";
 
+/*
+ 
+ */
+
+
+Calulate del = new Calulate(Add);
+WriteLine(del(1, 2));
+
+static int Add(int a, int b)
+{
+    return a + b;
+}
+
+public delegate int Calulate(int x, int y);
+
+
+
+
+
