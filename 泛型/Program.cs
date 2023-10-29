4e9319bc84cad98a762204b20b1a456cef780f4e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 14:03:00 +0800

    泛型

--- 泛型/Program.cs
+++ 泛型/Program.cs
@@ -1,3 +1,32 @@
 ﻿using static System.Console;
 
 Title = "泛型";
+
+
+
+Student<string> s1 = new Student<string>();
+s1.Id = "1";
+
+Student<int> s2 = new Student<int>();
+s2.Id = 1;
+
+
+
+
+
+class Student1
+{
+    public int Id { get; set; } 
+}
+
+
+class Student2
+{
+    public int Id { get; set; }
+}
+
+class Student<T>
+{
+    public T Id { get; set; }
+}
+
