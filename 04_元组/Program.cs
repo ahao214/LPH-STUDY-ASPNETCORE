a879ed7af6305cb399ee874cf54a7720be75948a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 18:29:47 +0800

    元组

--- 04_元组/Program.cs
+++ 04_元组/Program.cs
@@ -13,3 +13,35 @@ WriteLine($"x={x},y ={y}");
 (x, y) = (y, x);
 
 WriteLine($"x={x},y ={y}");
+
+
+// 更简洁的语法
+
+// 定义元祖
+(string, int) lubanone = ("鲁班七号", 12);
+
+var lubantwo = ("鲁班八号", 112);
+
+WriteLine($"姓名：{lubanone.Item1},年龄：{lubanone.Item2}");
+
+// 使用命名字段1
+var one = (name: "luba", age: 22);
+WriteLine($"姓名：{one.name},age:{one.age}");
+
+// 使用命名字段2
+(string name, int age) two = ("天空", 1111);
+WriteLine($"name:{two.name},age:{two.age}");
+
+
+// 元祖的解构
+(string name, int age) = ("纣王", 234);
+var (names, ages) = ("妲己", 11111);
+
+WriteLine($"name:{name},age:{age}");
+WriteLine($"name:{names},age:{ages}");
+
+
+
+
+
+
