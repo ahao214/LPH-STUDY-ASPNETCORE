81c73db8c1662c73b62b4d951478bf9b4492eb18
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 18:52:44 +0800

    统计方法

---
+++ 05_统计方法/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+Title = "统计方法";
+
+
+int[] arr = { 2, 3, 3, 3, 3, 31, 2, 345, 45, 34, 78, 123, 45, 67, 90, 8 };
+
+WriteLine($"和：{arr.Sum()},平均数：{arr.Average():0.00},数量：{arr.Count()},最大值：{arr.Max()},最小值：{arr.Min()}");
+
+
+
+
+
+
+
+
+
+
+
+
+
