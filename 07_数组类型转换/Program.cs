8d24327a4ee5563c57142ede030b38320f3edc34
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-30 23:09:02 +0800

    数组类型转换 1对1

--- 07_数组类型转换/Program.cs
+++ 07_数组类型转换/Program.cs
@@ -3,3 +3,20 @@
 Title = "数组类型转换";
 
 
+#region 数组类型转换 1对1
+
+// 方法一，循环处理 转化为字符串数组
+int[] arri = {12,3,4,5,6,798,90};
+string[] arrs = new string[arri.Length];
+for(int i = 0;i< arri.Length;i++)
+{
+    arrs[i] = arri[i].ToString();
+}
+
+
+// 方法二 使用Array.ConvertAll()
+int[] arrt = { 12, 3, 4, 5, 6, 798, 90 };
+string[] arrTS = Array.ConvertAll(arrt, i => i.ToString());
+double[] arrd = Array.ConvertAll(arri, i => Convert.ToDouble(i));
+
+#endregion
