56795153bc1efd48cbf8d985f47c5d4602ec9b66
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-31 21:44:05 +0800

    有参数有返回值Lambda

--- 09_Lambda表达式/Program.cs
+++ 09_Lambda表达式/Program.cs
@@ -13,6 +13,13 @@ Action<string> action1 = msg => WriteLine(msg);
 action1("有参数无返回值Lambda");
 
 
+// 有参数有返回值Lambda
+Func<int, int> func = (x) =>
+{
+    return x + 1;
+};
+WriteLine(func(99));
+
 
 
 
