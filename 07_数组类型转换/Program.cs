bdb9aadbfbcc7fdea067b264a2c42b1ecd0894b6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-31 16:55:15 +0800

    数组转化为byte[] 1对多

--- 07_数组类型转换/Program.cs
+++ 07_数组类型转换/Program.cs
@@ -25,7 +25,19 @@ double[] arrd = Array.ConvertAll(arri, i => Convert.ToDouble(i));
 
 #region 数组转化为byte[] 1对多
 
+// byte[] 是常用的一种数据类型 程序中经常需要转化为这种类型
+// BitConverter可将基本数据类型转换为一个字节数组
+// 也可将一个字节数组转换为基本数据类型
 
+int[] arrI = { 12, 23, 4, 5, 6, 21, 67, 98 };
+byte[] result = new byte[arrI.Length * sizeof(int)];
+for(int i = 0;i< arrI.Length;i++)
+{
+    byte[] temp = BitConverter.GetBytes(arrI[i]);
+    Array.Copy(temp, 0, result, i * sizeof(int), temp.Length);
+}
+
+// 更简单的方法
 
 
 #endregion
