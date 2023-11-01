f682b490c07db439710c9b3724ae8ed32fb3b11d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-02 00:01:53 +0800

    视图缩放

--- WpfConsolePannel/MainWindow.xaml.cs
+++ WpfConsolePannel/MainWindow.xaml.cs
@@ -24,5 +24,32 @@ namespace WpfConsolePannel
         {
             InitializeComponent();
         }
+
+
+        /// <summary>
+        /// 视图缩放
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Canvas_MouseWheel(object sender,MouseWheelEventArgs e)
+        {
+            // 获取实际渲染的高度和宽度
+            var width = this.mainView.ActualWidth + e.Delta;
+            var height = this.mainView.ActualHeight + e.Delta;
+            if (width < 100)
+                width = 500;
+            if (height < 100)
+                height = 100;
+            this.mainView.Height = height;
+            this.mainView.Width = width;
+            // 设置画板缩放的左边位置
+            this .mainView .SetValue (Canvas.LeftProperty,(this.RenderSize.Width - this.mainView.Width) / 2);
+        }
+
+
+        bool _isMoving = false;
+        Point _point = new Point(0, 0);
+        double _left =0, _top = 0;
+
     }
 }
