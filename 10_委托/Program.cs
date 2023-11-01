a8edf5c549391310983dac0e6e7b905703984bb8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-01 21:13:20 +0800

    委托

--- 10_委托/Program.cs
+++ 10_委托/Program.cs
@@ -6,8 +6,6 @@ Title = "委托";
  * 定义委托
  * 委托也是可以定义在类中
  * 委托与类是平级的，都是类型
- * 
- * 
  */
 
 static double Add(double x, double y)
@@ -20,21 +18,28 @@ static double Dec(double x, double y)
     return x - y;
 }
 
+static double Mul(double x, double y)
+{
+    return x * y;
+}
+
+static double Div(double x, double y)
+{
+    if (y == 0)
+        return 0;
+    return x / y;
+}
+
 static void Test(Cal f)
 {
-    Write("请输入：X");
+    WriteLine("请输入：X");
     double x = Convert.ToDouble(ReadLine());
-    Write("请输入：Y");
+    WriteLine("请输入：Y");
     double y = Convert.ToDouble(ReadLine());
 
     // 委托方法工作，怎么工作由具体委托决定
     double result = f(x, y);
-    WriteLine($"X:{x}与Y：{y}委托方法计算结果为：{result}");
-
-
-
-
-
+    WriteLine($"X:{x}与Y:{y}委托方法计算结果为：{result}");
 }
 
 
@@ -47,6 +52,9 @@ cal = new Cal(Dec);
 result = cal(5, 9);
 WriteLine(result);
 
+
+Cal calTest = Add;
+Test(calTest);
 
 
 public delegate double Cal(double x, double y);
