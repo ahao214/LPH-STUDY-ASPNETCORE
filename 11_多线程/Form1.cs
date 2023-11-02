23a0c68c8320b6990e648f2ebbcf55d86ee4b773
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-02 09:43:53 +0800

    单线程做菜

--- 11_多线程/Form1.cs
+++ 11_多线程/Form1.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+
+
 namespace _11_多线程
 {
     public partial class Form1 : Form
@@ -5,6 +8,39 @@ namespace _11_多线程
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 单线程做菜
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(2000);
+            MessageBox.Show("素菜做好了");
+            Thread.Sleep(5000);
+            MessageBox.Show("荤菜做好了");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
