c259c83a9edce1a2bcf31b611d1f724bea182fb7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-03 19:28:11 +0800

    Window窗体的生命周期

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace HelloWorld
         public MainWindow()
         {
             InitializeComponent();
+
+            this.SourceInitialized += (s, e) => Console.WriteLine("MainWindow.SourceInitialized 被触发");
+
+            this.Activated += (s, e) => Console.WriteLine("2.MainWindow.Activated 被触发");
+
+            this.Loaded += (s, e) => Console.WriteLine("3.MainWindow.Loaded 被触发");
+
+            this.ContentRendered += (s, e) => Console.WriteLine("4.MainWindow.ContentRendered 被触发");
+
+            this.Deactivated += (s, e) => Console.WriteLine("5.MainWindow.Deactivated 被触发");
+
+            this.Closing += (s, e) => Console.WriteLine("6.MainWindow.Closing 被触发");
+
+            this.Closed += (s, e) => Console.WriteLine("6.MainWindow.Closed 被触发");
+
+            this.Unloaded += (s, e) => Console.WriteLine("6.MainWindow.Unloaded 被触发");
+
         }
     }
 }
