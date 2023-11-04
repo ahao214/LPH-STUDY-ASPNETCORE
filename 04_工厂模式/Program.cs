9a82e4b0f023a335cbfe7d9bc8b6dea3ef58dfa1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-04 21:38:33 +0800

    设计模式-简单工厂、工厂模式、抽象工厂

--- 04_工厂模式/Program.cs
+++ 04_工厂模式/Program.cs
@@ -8,6 +8,9 @@ using System.Data;
  * 目的：隐藏创建对象的细节，根据条件创建想要的实例
  */
 
+var factory = new SqlServerFactory();
+
+
 
 #region 简单工厂
 
@@ -60,3 +63,47 @@ static class Factory
 
 
 #endregion
+
+
+
+abstract class AbstractionFactory
+{
+    public abstract IDAO Create();
+}
+
+class SqlServerFactory : AbstractionFactory
+{
+    public override IDAO Create()
+    {
+        return new SqlServerDAO();
+    }
+}
+
+
+class MySqlServerFactory : AbstractionFactory
+{
+    public override IDAO Create()
+    {
+        return new MySqlServerDAO();
+    }
+}
+
+
+abstract class AbsFactory
+{
+    public abstract IDAO CreateSqlServer();
+    public abstract IDAO CreateMySqlServer();
+}
+
+class ERP : AbsFactory
+{
+    public override IDAO CreateMySqlServer()
+    {
+        return new MySqlServerDAO();
+    }
+
+    public override IDAO CreateSqlServer()
+    {
+        return new SqlServerDAO();
+    }
+}
