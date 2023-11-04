88aa0344df2167be22594c40696e2f1c0b7adb08
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-04 21:51:31 +0800

    设计模式：外观模式

---
+++ 06_外观模式/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+
+/*
+设计模式：外观模式
+目的：隐藏复杂的实现
+*/
+
+
+
+
+
+interface IUserRepository
+{
+    object GetById(int id);
+}
+
+interface IRoleRepository
+{
+    IEnumerable<string> GetRoles(in int userId);
+}
+
+interface IAuthorityRepository
+{
+    IEnumerable<string> GetAuthorities(IEnumerable<string> roles);
+}
