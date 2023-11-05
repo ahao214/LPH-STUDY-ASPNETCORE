74562db3ca98210184d8800d8ce6a808948c17ff
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-05 14:15:47 +0800

    多播委托

--- 多播委托/Program.cs
+++ 多播委托/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using static System.Console;
+
+Title = "多播委托";
+
+
+
+Calculate cal = Add;
+cal += Sub; // 添加方法
+cal(5, 2);  // 委托方法调用
+
+
+
+static void Add(int a, int b)
+{
+    WriteLine(a + b);
+}
+
+static void Sub(int a, int b)
+{
+    WriteLine(a - b);
+}
+
+
+
+delegate void Calculate(int a, int b);
+
