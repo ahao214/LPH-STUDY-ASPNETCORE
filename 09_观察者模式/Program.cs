9163766cd7a1dd926cafeb879b1d677ed516268c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-05 20:42:34 +0800

    观察者模式

--- 09_观察者模式/Program.cs
+++ 09_观察者模式/Program.cs
@@ -6,3 +6,50 @@ Title = "观察者模式";
 设计模式：观察者模式
 目的：让下面的对象一致的行动
 */
+
+var t = new Teacher();
+t.Add(new Action1());
+t.Add(new Action2());
+
+// 调用观察者的指令
+t.Command();
+
+
+
+interface IAction
+{
+    void Do();
+}
+
+
+class Action1 : IAction
+{
+    public void Do() => WriteLine("Action1 做了行为");
+}
+
+class Action2 : IAction
+{
+    public void Do() => WriteLine("Action2 做了行为");
+}
+
+
+class Teacher
+{
+    List<IAction> actions = new List<IAction>();
+
+    public void Add(IAction action)
+    {
+        actions.Add(action);
+    }
+
+    public void Remove(IAction action)
+    {
+        actions.Remove(action);
+    }
+
+    public void Command()
+    {
+        actions.ForEach(action => action.Do());
+    }
+
+}
