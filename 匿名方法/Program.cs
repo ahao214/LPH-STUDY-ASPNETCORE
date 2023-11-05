965759c66b2f6d4e2ef205ba0b95e9242d634b08
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-05 14:07:07 +0800

    匿名方法

--- 匿名方法/Program.cs
+++ 匿名方法/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using static System.Console;
+
+Title = "匿名方法";
+
+
+
+Calculate cal = delegate (int a, int b)
+{
+    return a + b;
+};
+
+var result = cal(1, 2);
+WriteLine(result);
+
+public delegate int Calculate(int a, int b);
