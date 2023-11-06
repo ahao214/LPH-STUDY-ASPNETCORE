281517ca9f1451517a7552322edb9ed6fbd96c49
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 16:40:32 +0800

    发送信息

--- Chat/wwwroot/js/chat.js
+++ Chat/wwwroot/js/chat.js
@@ -1,9 +1,22 @@
 ﻿// 创建并启动连接
 let conn = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-conn.start();
+$("#btnSend").attr("disabled", true);
+conn.start().then(function () {
+    $("#btnSend").attr("disabled", false);
+}).catch(function (err) {
+    alert("链接signalR服务器失败")
+    return console.log(err)
+})
 
 
 // 发送信息
+$("#btnSend").click(function () {
+    let user = $("#user").val()
+    let msg = $("#msg").val()
+    conn.invoke("SendMsg", user, msg).catch(function (err) {
+        return console.log(err)
+    })
+})
 
 
 // 接收信息
