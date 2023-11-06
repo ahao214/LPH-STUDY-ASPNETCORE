9d7ad627218bb2d461ace61c8bebe8b0d72a4494
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 13:36:49 +0800

    节点配置实体类

---
+++ T2/Utils/SiteConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace T2.Utils
+{
+    /// <summary>
+    /// 节点配置实体类
+    /// </summary>
+    public class SiteConfig
+    {
+        public string SiteName { get; set; }
+        public string SiteCopyRight { get; set; }
+    }
+}
