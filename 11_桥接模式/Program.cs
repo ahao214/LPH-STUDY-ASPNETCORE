224484f988b36b572105147226c38a02db191a9e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 18:40:09 +0800

    桥接模式

---
+++ 11_桥接模式/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+// 设计模式：桥接模式
+// 目的：双方独立，且又能关联
+
+
+//DbContext
+
+
+
+
+interface IRepository<TEntity> where TEntity: class
+{
+    void Add(TEntity entity);
+    void Remove(TEntity entity);
+}
+
