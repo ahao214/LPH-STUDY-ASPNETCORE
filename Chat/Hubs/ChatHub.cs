bdf354da4149c2ec7b4829485dc8c03b1e89e5d6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 16:19:54 +0800

    发送信息

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+
+    public class ChatHub : Hub
+    {
+        /// <summary>
+        /// 发送信息
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public async Task SendMsg(string user, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMsg", user, msg);
+        }
+
+    }
+}
