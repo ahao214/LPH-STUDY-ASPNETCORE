0417bca4dba0bc2833a92503f3382432c58340f0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 21:48:28 +0800

    获取xml文件的数据

--- TestXML/MainWindow.xaml.cs
+++ TestXML/MainWindow.xaml.cs
@@ -28,6 +28,25 @@ namespace TestXML
             string path = @"..\..\test.xml";
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
+            XmlNode xmlNode = doc.SelectSingleNode("StudentInfo");
+
+            XmlNodeList xmlNodeList = xmlNode.ChildNodes;
+            List <Student > lstStudent = new List <Student> ();   
+            foreach (XmlNode item in xmlNodeList )
+            {
+                Student student = new Student();
+                XmlElement ele = (XmlElement)item;
+                student.Grade = ele.GetAttribute("Grade");
+                student.Teacher = ele.GetAttribute("Teacher");
+
+                XmlNodeList xmlNodeList1 = ele.ChildNodes;
+                student.Name = xmlNodeList1.Item(0).InnerText;
+                student.Address = xmlNodeList1.Item(1).InnerText;
+                student.Age = Convert.ToInt32(xmlNodeList1.Item(2).InnerText);
+                lstStudent.Add(student);
+
+
+            }
 
         }
     }
