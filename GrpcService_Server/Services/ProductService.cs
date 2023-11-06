f8dd0b23b25aa4a5a1de730cfd3dfbe1b93a89b9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 15:23:08 +0800

    创建ProductService类

---
+++ GrpcService_Server/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcService_Product_Server;
+
+namespace GrpcService_Server.Services
+{
+    public class ProductService:ProductManager.ProductManagerBase
+    {
+        public override Task<ProductReply> GetProductByID(ProductRequest request, ServerCallContext context)
+        {
+            return base.GetProductByID(request, context);
+        }
+
+
+        public override Task<AllProductsReply> GetProductAll(Empty request, ServerCallContext context)
+        {
+            return base.GetProductAll(request, context);
+        }
+
+    }
+}
