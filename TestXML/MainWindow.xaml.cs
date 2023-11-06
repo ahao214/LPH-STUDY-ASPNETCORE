61676b16d2f0aee685d8a273de25d51d37472818
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 21:58:20 +0800

    忽略XML文件中的注释

--- TestXML/MainWindow.xaml.cs
+++ TestXML/MainWindow.xaml.cs
@@ -27,32 +27,59 @@ namespace TestXML
 
             string path = @"..\..\test.xml";
             XmlDocument doc = new XmlDocument();
-            doc.Load(path);
+
+            //doc.Load(path);
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            // 忽略XML文件中的注释
+            settings.IgnoreWhitespace = true;
+            XmlReader xmlReader = XmlReader.Create(path, settings);
+            doc.Load(xmlReader);
+
             XmlNode xmlNode = doc.SelectSingleNode("StudentInfo");
 
             XmlNodeList xmlNodeList = xmlNode.ChildNodes;
             List<Student> lstStudent = new List<Student>();
             foreach (XmlNode item in xmlNodeList)
             {
-                // 读取到的信息是注释
-                if (item is XmlComment)
-                {
-                    XmlComment comment = (XmlComment)item;
-                    string str = comment.InnerText;
-                }
-                else
-                {
-                    Student student = new Student();
-                    XmlElement ele = (XmlElement)item;
-                    student.Grade = ele.GetAttribute("Grade");
-                    student.Teacher = ele.GetAttribute("Teacher");
+                #region
 
-                    XmlNodeList xmlNodeList1 = ele.ChildNodes;
-                    student.Name = xmlNodeList1.Item(0).InnerText;
-                    student.Address = xmlNodeList1.Item(1).InnerText;
-                    student.Age = Convert.ToInt32(xmlNodeList1.Item(2).InnerText);
-                    lstStudent.Add(student);
-                }
+                // 读取到的信息是注释
+                //if (item is XmlComment)
+                //{
+                //    XmlComment comment = (XmlComment)item;
+                //    string str = comment.InnerText;
+                //}
+                //else
+                //{
+                //    Student student = new Student();
+                //    XmlElement ele = (XmlElement)item;
+                //    student.Grade = ele.GetAttribute("Grade");
+                //    student.Teacher = ele.GetAttribute("Teacher");
+
+                //    XmlNodeList xmlNodeList1 = ele.ChildNodes;
+                //    student.Name = xmlNodeList1.Item(0).InnerText;
+                //    student.Address = xmlNodeList1.Item(1).InnerText;
+                //    student.Age = Convert.ToInt32(xmlNodeList1.Item(2).InnerText);
+                //    lstStudent.Add(student);
+                //}
+
+                #endregion
+
+                #region 忽略XML文件中的注释
+
+                Student student = new Student();
+                XmlElement ele = (XmlElement)item;
+                student.Grade = ele.GetAttribute("Grade");
+                student.Teacher = ele.GetAttribute("Teacher");
+
+                XmlNodeList xmlNodeList1 = ele.ChildNodes;
+                student.Name = xmlNodeList1.Item(0).InnerText;
+                student.Address = xmlNodeList1.Item(1).InnerText;
+                student.Age = Convert.ToInt32(xmlNodeList1.Item(2).InnerText);
+                lstStudent.Add(student);
+
+                #endregion
             }
 
             listView.ItemsSource = lstStudent;
