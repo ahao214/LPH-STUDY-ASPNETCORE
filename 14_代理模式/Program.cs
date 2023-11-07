46ced4cfdc3c3e2dfcb39255374687c89e225e03
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 11:20:23 +0800

    代理模式

--- 14_代理模式/Program.cs
+++ 14_代理模式/Program.cs
@@ -3,3 +3,38 @@
 
 // 设计模式：代理模式
 // 目的：帮你完成你没有完成的内容
+
+
+var real = new RealAction();
+var proxy = new ProxyAction(real);
+
+proxy.Do();
+
+
+interface IAction
+{
+    void Do();
+}
+
+
+class RealAction : IAction
+{
+    public void Do() => WriteLine("我是真的");
+}
+
+class ProxyAction:IAction
+{
+    private readonly IAction _action;
+    public ProxyAction(IAction action)
+    {
+        _action = action;
+    }
+
+    public void Do()
+    {
+        WriteLine("我在真的之前");
+        _action.Do();
+        WriteLine("我在真的之后");
+    }
+
+}
