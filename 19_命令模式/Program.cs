7507b22ff26d8e0c7b4746d016eb85e0b75e1550
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 12:12:59 +0800

    命令模式

---
+++ 19_命令模式/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+// 设计模式：命令模式
+
+var person = new Person();
+var command = new WashCommand(person);
+command.Execute();
+
+
+class Person
+{
+    public void WashHand()
+    {
+        WriteLine("洗手");
+    }
+}
+
+
+interface ICommand
+{
+    void Execute();
+}
+
+class WashCommand : ICommand
+{
+    private readonly Person _person;
+    public WashCommand(Person person)
+    {
+        _person = person;
+    }
+
+    public void Execute()
+    {
+        _person.WashHand();
+    }
+}
+
+
+
