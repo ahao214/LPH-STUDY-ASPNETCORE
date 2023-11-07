9398f6f44e56083bf388e2785afeb9ad17149217
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 12:33:19 +0800

    ASP.NET Core的运行生命周期

--- 01_Host/Program.cs
+++ 01_Host/Program.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Extensions.Hosting;
 
 
+/*
+ ASP.NET Core的运行生命周期
+Program->各种环境的配置->配置服务(依赖注入)->构建ApplicationBuilder->中间件->运行/结束->Program
+ */
+
 var builder = Host.CreateApplicationBuilder(args);
 var host = builder .Build();
 
