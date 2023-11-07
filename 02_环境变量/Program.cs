3312c5fffe583d53b03ae3c10f900cb60e0b48d2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 12:51:27 +0800

    环境变量

---
+++ 02_环境变量/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Memory;
+
+// 环境变量
+
+var builder = Host.CreateDefaultBuilder();
+builder.UseEnvironment("UAT");
+
+builder.ConfigureHostConfiguration(configure =>
+{
+    configure.AddEnvironmentVariables("myenv")
+    .AddJsonFile("test.json");
+});
+
+var host = builder.Build();
+var config = host.Services.GetRequiredService<IConfiguration>();
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("My ENV IS:{0}", config.GetValue<string>("key"));
+
+host.Run();
+
+
+
