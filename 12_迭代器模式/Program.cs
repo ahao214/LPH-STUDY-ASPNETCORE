24562cb0ccf91f9d9d480f07a4117f8df9eb4e6e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 09:42:30 +0800

    迭代器模式

--- 12_迭代器模式/Program.cs
+++ 12_迭代器模式/Program.cs
@@ -3,3 +3,38 @@
 // 设计模式：迭代器模式
 // 目的：循环遍历输出结果
 
+
+
+interface IIterator
+{
+    bool HasNext();
+    object Next();
+}
+
+class UserCollection : IIterator
+{
+    User[] _users;
+    int index;
+    public UserCollection(User[] users)
+    {
+        _users = users;
+    }
+
+    public bool HasNext()
+    {
+        return index < _users.Length;
+    }
+
+    public object Next()
+    {
+        var instance = _users[index];
+        index++;
+        return instance;
+    }
+}
+
+
+class User
+{
+
+}
