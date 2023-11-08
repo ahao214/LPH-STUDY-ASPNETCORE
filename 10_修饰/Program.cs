3d8c45f87975ce608bd7229028a4755f625ef267
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 21:39:03 +0800

    init

--- 10_修饰/Program.cs
+++ 10_修饰/Program.cs
@@ -1,1 +1,19 @@
 ﻿// record required init 修饰
+
+
+// init 属性必须在 new 的时候初始化(赋值)
+// initial 
+
+var user = new User()
+{
+    Id = 1,
+    Name = "jack",
+    Age = 5
+};
+
+class User
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public int Age { get; init; }
+}
