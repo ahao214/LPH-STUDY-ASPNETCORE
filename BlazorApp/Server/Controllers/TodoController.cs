732c197ad11ee48857eaf8bf860dc6be4f7a4ce1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 16:31:45 +0800

    方法

---
+++ BlazorApp/Server/Controllers/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApp.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Server.Controllers
+{
+    [Route("api/todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IResult> GetAsync()
+        {
+            var list = await _context.Todos.ToListAsync();
+            return Results.Ok(list);
+        }
+    }
+}
