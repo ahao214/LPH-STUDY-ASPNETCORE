4f7f5564cbe0c8032502ec9e4a336c3a8d347731
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 12:37:00 +0800

    ListView

--- WpfListView/MainWindow.xaml.cs
+++ WpfListView/MainWindow.xaml.cs
@@ -20,14 +20,33 @@ namespace WpfListView
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class Person
+        {
+            public string? Name { get; set; }
+            public int Age { get; set; }
+            public string? Address { get; set; }
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
+            listview.Items.Add(new Person { Name = "张三", Age = 22, Address = "广东省廉江市车板镇大坝村" });
+            listview.Items.Add(new Person { Name = "李四", Age = 23, Address = "江西省景德镇市市辖区" });
+            listview.Items.Add(new Person { Name = "王五", Age = 24, Address = "上海市虹口区" });
+        }
         private void listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            ListView listView = sender as ListView;
+            if (listView == null) return;
 
+            var person = listView.SelectedItem as Person;
+            if (person == null) return;
+
+            _TextBlockName.Text = person.Name;
+            _TextBlockAge.Text = person.Age + "岁";
+            _TextBlockAddress.Text = person.Address;
         }
     }
 }
