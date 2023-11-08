0e3e9d7e9cf3b2a05b86f518fe2ebdcc91db6d77
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 16:08:19 +0800

    单元测试

---
+++ AspnetTest/LoginControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreLifetime.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspnetTest
+{
+    public class LoginControllerTest
+    {
+        [Fact]
+        public void Test_Login()
+        {
+            var controller = new LoginController();
+
+           var result = controller.Login(new() { UserName = "admin" });
+            Assert.IsType<OkResult>(result);
+
+
+        }
+
+
+    }
+}
