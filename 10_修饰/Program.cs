69bfdce7b0ac5a72eb8253e7f93c27760141580f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 21:44:01 +0800

    record

--- 10_修饰/Program.cs
+++ 10_修饰/Program.cs
@@ -3,16 +3,29 @@
 
 // init 属性只能在 new 的时候初始化(赋值)
 // initial 
-
-
-// required 属性必须在 new 的时候初始化(赋值)
-
 var user = new User()
 {
     Id = 1,
     Name = "jack",
     Age = 5
 };
+
+// required 属性必须在 new 的时候初始化(赋值)
+
+// record 修饰不可变的对象
+
+// DDD: 值对象
+
+var teacher = new Teacher("admin", "111");
+
+
+
+record class Teacher(string Name,string Pwd);
+
+
+
+
+
 
 class User
 {
