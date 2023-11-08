653e8d7847e1d2233ec7c3fd8037a436932736cd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 15:37:29 +0800

    过滤器

---
+++ AspNetCoreLifetime/LogFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCoreLifetime
+{
+    public class LogFilter:IActionFilter
+    {
+        private readonly ILogger<LogFilter> _logger;
+
+        public LogFilter(ILogger<LogFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation($"Action:{context.ActionDescriptor.DisplayName} 执行完毕");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation($"当前执行：{context.ActionDescriptor.DisplayName}");
+        }
+    }
+}
--- AspNetCoreLifetime/Program.cs
+++ AspNetCoreLifetime/Program.cs
@@ -22,4 +22,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+
+
 app.Run();
