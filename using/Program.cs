907342a09244956de9ed746ec4c3ca48bd448072
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 19:01:51 +0800

    using 关键字的用法

--- using/Program.cs
+++ using/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+using static System.Math;
 
 // using 关键字的用法
 
@@ -9,8 +10,15 @@
 
 // 3.给类一个别名
 
+// 4.using static 静态命名
+
+// 5.global using 全局引用
+
+
 using MyClass1 = Name1.MyClass;
 using MyClass2 = Name2.MyClass;
+using S = System;
+
 
 var myClass1 = new MyClass1();
 var myClass2 = new MyClass2();  
@@ -24,3 +32,5 @@ namespace Name2
 {
     class MyClass { }
 }
+
+
