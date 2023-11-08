207acc7a369d79179b91293f57aa6ec00c5d81fd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 20:51:42 +0800

    反射

---
+++ 05_反射/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+// 反射
+// 一切从 Type 开始
+// 动态操作对象
+
+
+var user = new User();
+user.Name = "tom";
+user.Id = 1;
+
+user.GetType()
+    .GetProperties()
+    .ToList()
+    .ForEach(item =>
+    {
+        var name = item.Name;
+        item.GetValue(user);
+    });
+
+
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
