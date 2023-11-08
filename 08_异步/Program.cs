cce914c624299af61875aae8deb64d7e08d8118a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 21:33:39 +0800

    异步

---
+++ 08_异步/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using System.Threading;
+
+// 异步
+
+
+Thread.Sleep(5000);
+WriteLine("beginning");
+await GetData();
+WriteLine("end");
+
+ReadKey();
+
+static Task GetData()
+{
+    return Task.Run(() =>
+    {
+        Thread.Sleep(2000);
+        WriteLine("获取到数据");
+    });
+}
