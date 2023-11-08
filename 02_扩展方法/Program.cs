c80104cdeb84ae48d2ccae2733382a5ef25f8dee
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 19:06:19 +0800

    扩展方法

--- 02_扩展方法/Program.cs
+++ 02_扩展方法/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 // 扩展方法
+// 扩展方法必须写在静态类中，并且该类中的方法必须是静态方法
+// 如何给string增加一些方法
+
+
 
 var text = "";
 
@@ -8,11 +12,18 @@ if (text.IsNotNullOrEmpty())
 
 }
 
+text.Append("").Append("");
+
 
 public static class StringExtensions
 {
     public static bool IsNotNullOrEmpty(this string value)
     {
         return !string.IsNullOrEmpty(value);
+    }
+
+    public static string Append(this string value,string text)
+    {
+        return value + text;
     }
 }
