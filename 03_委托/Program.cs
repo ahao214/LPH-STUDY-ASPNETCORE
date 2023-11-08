789af1b4f9fbc604850892a0e93ffa3926f0a5a5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 20:33:21 +0800

    委托

--- 03_委托/Program.cs
+++ 03_委托/Program.cs
@@ -4,5 +4,39 @@
 // 参数 属性
 
 
+var cal = new Calculator();
+var result =cal.DoAdd(MakePlus);
+
+cal.DoAdd((x, y) =>
+{
+    return x + y;
+});
 
 
+static int MakePlus(int a, int b)
+{
+    return a + b;
+}
+
+
+int Add(int a,int b)
+{
+    return a + b;
+}
+
+
+class Calculator
+{
+    public delegate int Add(int a, int b);
+
+    public int DoAdd(Add action)
+    {
+        return action(1, 2);
+
+    }
+
+    public int AddMethod(int a, int b)
+    {
+        return a + b;
+    }
+}
