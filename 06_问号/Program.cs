c05e28326d84c096b053c43505c43611c8e2cdfe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 21:00:40 +0800

    问号

---
+++ 06_问号/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+// ? 表达式
+
+// 1.三元运算符
+
+var result = true ? 1 : 2;
+
+// not null ?? is null
+
+string name = default;
+
+// name 是null走右边 不是null的时候走左边
+var str = name ?? "abc";
+
+public class C1
+{
+    public C1(string name)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+    }
+    public string Name { get; }
+}
