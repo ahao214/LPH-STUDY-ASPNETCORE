68cf8a6ea4778a40ca2c3f4d7810b0af5b07b82a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 16:23:06 +0800

    Todo

--- BlazorApp/Server/Data/Todo.cs
+++ BlazorApp/Server/Data/Todo.cs
@@ -5,13 +5,25 @@ namespace BlazorApp.Server.Data
 {
     public class Todo
     {
+        /// <summary>
+        /// 自增列
+        /// </summary>
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]      
         public int Id { get; set; }
+        /// <summary>
+        /// 标题名称
+        /// </summary>
         [Required,StringLength(50)]
         public string Name { get; set; }
+        /// <summary>
+        /// 任务描述
+        /// </summary>
         [StringLength(255)]
         public string? Description { get; set; }
+        /// <summary>
+        /// 计划时间
+        /// </summary>
         public DateTime? PlanTime { get; set; }
 
     }
