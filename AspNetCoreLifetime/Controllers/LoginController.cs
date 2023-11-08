d01663aa5231b1b3fee22ed311c8d88e0b7ab16b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 15:22:25 +0800

    数据处理

---
+++ AspNetCoreLifetime/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreLifetime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IResult Login([FromForm]LoginModel model)
+        {
+            return Results.Ok(model);
+        }
+
+    }
+
+
+
+    public class LoginModel
+    {
+        public string? UserName     { get; set; }
+        public string? Password { get; set; }
+    }
+}
