297ebe5a617b403a740829cafce8de8ddd1d6e3f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 19:04:19 +0800

    扩展方法

---
+++ 02_扩展方法/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// 扩展方法
+
+var text = "";
+
+if (text.IsNotNullOrEmpty())
+{
+
+}
+
+
+public static class StringExtensions
+{
+    public static bool IsNotNullOrEmpty(this string value)
+    {
+        return !string.IsNullOrEmpty(value);
+    }
+}
