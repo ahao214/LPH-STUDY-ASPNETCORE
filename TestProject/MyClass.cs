b520f76afe31fab370d93645f2c099f98c8d67ec
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 21:48:52 +0800

    单元测试

---
+++ TestProject/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class MyClass
+    {
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+
+        public int SumAll(int[]x,int[]y)
+        {
+            var sum = 0;
+            foreach (var x2 in x)
+            {
+                foreach (var y2 in y)
+                {
+                    sum += Sum(x2, y2);
+                }
+            }
+            return sum;
+        }
+
+
+    }
+}
