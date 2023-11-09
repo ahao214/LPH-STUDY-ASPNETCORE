20ac889cae1e8df77b4ed8b619bf5e6d17237e0e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 21:19:11 +0800

    序列化和反序列化

--- 13_Json/Program.cs
+++ 13_Json/Program.cs
@@ -10,21 +10,47 @@ using System.Text.Unicode;
 
 // JSON
 
-var user = new UserInfo("jack", 23, new[] { "开发者" });
-var json = JsonSerializer.Serialize(user,new JsonSerializerOptions
+//var user = new UserInfo("jack", 23, new[] { "开发者" });
+//var json = JsonSerializer.Serialize(user,new JsonSerializerOptions
+//{
+//       Encoder=JavaScriptEncoder.Create(UnicodeRanges.All)
+//});
+//WriteLine(json);
+
+//// 反序列化
+//var users = JsonSerializer.Deserialize<UserInfo>(json);
+//WriteLine(user);
+//WriteLine($"Name:{users.Name}");
+//WriteLine($"Age:{users.Age}");
+//WriteLine($"Roles:{string.Join(",", users.Roles)}");
+
+
+var readers = new StreamReader("demo.json");
+var jsons = readers.ReadToEnd();
+var list = JsonSerializer.Deserialize<List<UserInfo>>(jsons);
+foreach (var u in list)
 {
-       Encoder=JavaScriptEncoder.Create(UnicodeRanges.All)
-});
-WriteLine(json);
-
-// 反序列化
-var users = JsonSerializer.Deserialize<UserInfo>(json);
-WriteLine(user);
-WriteLine($"Name:{users.Name}");
-WriteLine($"Age:{users.Age}");
-WriteLine($"Roles:{string.Join(",", users.Roles)}");
+    WriteLine($"Name:{u.Name}");
+    WriteLine($"Age:{u.Age}");
+    WriteLine($"Roles:{string.Join(",", u.Roles)}");
+}
 
 
-public record UserInfo(string Name,int Age, string[] Roles);
+// 读取demo1.json
+var reader1 = new StreamReader("demo1.json");
+var json1 = reader1.ReadToEnd();
+var list1 = JsonSerializer.Deserialize<Dictionary<string, UserInfo>>(json1);
+foreach (var u1 in list1)
+{
+    WriteLine($"Id:{u1.Key}");
+    WriteLine($"Name:{u1.Value.Name}");
+    WriteLine($"Age:{u1.Value.Age}");
+    WriteLine($"Roles:{string.Join(",", u1.Value.Roles)}");
+}
+
+
+
+
+public record UserInfo(string Name, int Age, string[] Roles);
 
 
