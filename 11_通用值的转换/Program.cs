a4b630ae62da55406cc4da42094b540da5285bfd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 20:33:32 +0800

    通用值的转换

--- 11_通用值的转换/Program.cs
+++ 11_通用值的转换/Program.cs
@@ -2,5 +2,25 @@
 
 // 通用值的转换
 
+var result = "2023-1-1".Parse<DateTime?>();
+
+WriteLine($"值是：{result},数据类型是：{result.GetType().Name}");
+
+ReadKey();
 
 
+public static class Extensions
+{
+    public static T Parse<T>(this object value)
+    {
+        var type = typeof(T);
+        var nullableType = Nullable.GetUnderlyingType(type);
+        if (nullableType is not null)
+        {
+            return (T)Convert.ChangeType(value, nullableType);
+        }
+
+        return (T)Convert.ChangeType(value, type);
+    }
+
+}
