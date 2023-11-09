1a1e55e705606ca86ed888528f0566dcf538e7b4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 13:52:48 +0800

    接口

--- Models/IProvider.cs
+++ Models/IProvider.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal interface IProvider
+    public interface IProvider<T> where T : class
     {
+        /// <summary>
+        /// 查询所有记录
+        /// </summary>
+        /// <returns></returns>
+        List<T> Select();
+        /// <summary>
+        /// 插入一条记录
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        int Insert(T t);
+        /// <summary>
+        /// 更新一条记录
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        int Update(T t);
+        /// <summary>
+        /// 删除一条记录
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        int Delete(T t);
+
+
     }
 }
