e3b6256973b5f8d2c1e201634d76a46699fb651c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 20:47:55 +0800

    分页

---
+++ 12_Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+
+int pageIndex = 1;
+int pageSize = 10;
+List<string> dataSource = new List<string>();
+do
+{
+    Write("请输入页码：");
+    pageIndex = int.Parse(ReadLine());
+
+    if (pageIndex < 0)
+    {
+        break;
+    }
+
+    var pagedSource = dataSource.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+    WriteLine($"第{pageIndex}页数据：{string.Join(",", pagedSource)}");
+} while (pageIndex > 0);
+
+WriteLine("退出");
+
