3cd7b7f84d24ceb46b0256a65676a5f49647980a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 14:54:30 +0800

    LoginViewModel

---
+++ AHOCMS/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHOCMS.ViewModel
+{
+    public class LoginViewModel
+    {
+
+
+    }
+}
