44e41362d91b8993d795f64412416a3a8cd6bc8a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-09 13:58:43 +0800

    美化窗体

--- AHOCMS/Windows/LoginWindow.xaml.cs
+++ AHOCMS/Windows/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace AHOCMS.Windows
     /// <summary>
     /// LoginWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : MetroWindow
     {
         public LoginWindow()
         {
