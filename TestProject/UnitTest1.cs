ffd821a5dff8e154ad6d7e0a6194bbb2d4926e79
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-10 10:30:02 +0800

    单元测试

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,10 +1,18 @@
 namespace TestProject
 {
-    public class UnitTest1
+    public class UnitTest1  // 测试套件
     {
         [Fact]
-        public void Test1()
+        public void Test1() // 测试用例
         {
+            //1、测试条件
+            //2、测试步骤
+            //3、期待结果
+
+            MyClass myClass = new();
+            var result = myClass.Sum(1, 2);
+            Assert.Equal(3, result);
+
 
         }
     }
