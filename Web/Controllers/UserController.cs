33d11a7b36bd08ceb486e2e29332048c61b53f06
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-10 14:57:02 +0800

    UserController

---
+++ Web/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Data;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public MyDbContext Context { get; set; }
+        public UserController(MyDbContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = Context.Users.ToList();
+            return Ok(users);
+        }
+
+
+
+    }
+}
