a003698206be32c6d3d54725456502499f59b13a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-12 20:26:07 +0800

    注册MongoDbDatabase

--- YDT_MongoDEbusiness/Program.cs
+++ YDT_MongoDEbusiness/Program.cs
@@ -1,3 +1,5 @@
+using YDT_MongoDEbusiness.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//1.»ñÈ¡Configuration
+var configuration = builder.Configuration;
+
+//2.×¢²áMongoDbDatabaseµ½IOCÈÝÆ÷
+builder.Services.AddSingleton<MongoDbDatabase>();
+
+
+
 
 var app = builder.Build();
 
