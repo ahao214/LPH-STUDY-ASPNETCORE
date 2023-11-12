3cbb563e0423297b04bd8e3450600eccc60e8151
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-12 20:10:12 +0800

    订单

---
+++ YDT_MongoDEbusiness/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YDT_MongoDEbusiness.Models;
+using MongoDB.Driver;
+
+
+namespace YDT_MongoDEbusiness.Controllers
+{
+    /// <summary>
+    /// 用户控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        IMongoCollection<Order> orders;
+        public OrderController()
+        {
+            //1.创建MongoClient
+            MongoClient mongoClient = new MongoClient("mongodb://localhost:2222");
+
+            //2.获取数据库            
+            var database = mongoClient.GetDatabase("数据库名称");
+
+            //3.获取数据库集合
+            orders = database.GetCollection<Order>("order");
+        }
+
+
+        /// <summary>
+        /// 添加订单方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost(Name = "AddOrder")]
+        public string AddOrder(Order order)
+        {
+            //4.存储用户数据到集合
+            orders.InsertOne(order);
+
+            return "添加成功";
+        }
+
+        /// <summary>
+        /// 查询订单
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "GetOrder")]
+        public List<Order> GetOrder()
+        {
+            //4.存储用户数据到集合
+            var orderList = orders.Find(o => o.Id == 1).ToList();
+
+            return orderList;
+        }
+
+    }
+}
