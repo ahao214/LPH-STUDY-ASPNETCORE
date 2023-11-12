2100df8077e8c1f5676dce66ff65672ca7de3046
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-12 20:15:28 +0800

    MongoDB复用类

--- YDT_MongoDEbusiness/Controllers/MongoDbDatabase.cs
+++ YDT_MongoDEbusiness/Controllers/MongoDbDatabase.cs
@@ -1,15 +1,23 @@
-﻿namespace YDT_MongoDEbusiness.Controllers
+﻿using MongoDB.Driver;
+
+
+namespace YDT_MongoDEbusiness.Controllers
 {
     /// <summary>
     /// MongoDB复用类
     /// </summary>
     public class MongoDbDatabase
     {
-        //1.创建MongoClient
-        MongoClient mongoClient = new MongoClient("mongodb://localhost:2222");
+        public IMongoDatabase database { get; }
 
-        //2.获取数据库            
-        var database = mongoClient.GetDatabase("数据库名称");
+        public MongoDbDatabase()
+        {
+            //1.创建MongoClient
+            MongoClient mongoClient = new MongoClient("mongodb://localhost:2222");
+
+            //2.获取数据库            
+            database = mongoClient.GetDatabase("数据库名称");
+        }
 
 
     }
