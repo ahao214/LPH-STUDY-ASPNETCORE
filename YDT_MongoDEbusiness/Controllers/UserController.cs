7ee4a0a1b4514b620cb8fd01ae8d21b2dd917ace
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-12 20:03:04 +0800

    查询用户

--- YDT_MongoDEbusiness/Controllers/UserController.cs
+++ YDT_MongoDEbusiness/Controllers/UserController.cs
@@ -18,7 +18,7 @@ namespace YDT_MongoDEbusiness.Controllers
         /// 添加用户方法
         /// </summary>
         /// <returns></returns>
-        [HttpPost(Name ="AddUser")]
+        [HttpPost(Name = "AddUser")]
         public string AddUser(User user)
         {
             //1.创建MongoClient
@@ -34,6 +34,28 @@ namespace YDT_MongoDEbusiness.Controllers
             users.InsertOne(user);
 
             return "添加成功";
+        }
+
+        /// <summary>
+        /// 查询用户
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "GetUser")]
+        public List <User> GetUser()
+        {
+            //1.创建MongoClient
+            MongoClient mongoClient = new MongoClient("mongodb://localhost:2222");
+
+            //2.获取数据库            
+            var database = mongoClient.GetDatabase("数据库名称");
+
+            //3.获取数据库集合
+            var users = database.GetCollection<User>("user");
+
+            //4.存储用户数据到集合
+            var userList = users.Find(u => u.Id == 1).ToList();
+
+            return userList;
         }
 
     }
