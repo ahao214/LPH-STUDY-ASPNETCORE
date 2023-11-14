c77f209eae5d63d4798cd04b8228d600bb352cba
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 16:56:44 +0800

    消息队列的入队和出队

--- MyRedisMq/MQ/RedisMessageQueue.cs
+++ MyRedisMq/MQ/RedisMessageQueue.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Encodings;
+
 
 namespace MyRedisMq.MQ
 {
@@ -11,6 +14,51 @@ namespace MyRedisMq.MQ
     /// </summary>
     public class RedisMessageQueue : IDisposable
     {
+        public RedisClient redisClient { get; }
+
+        public RedisMessageQueue(string redisHost)
+        {
+            this.redisClient = new RedisClient(redisHost);
+        }
+
+        /// <summary>
+        ///  入队
+        /// </summary>
+        /// <param name="QKey"></param>
+        /// <param name="QMessage"></param>
+        /// <returns></returns>
+        public long EnQueue(string QKey, string QMessage)
+        {
+            //1 编码字符串
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(QMessage);
+
+            //2 redis消息队列入队
+            long count = redisClient.LPush(QKey, bytes);
+            return count;
+        }
+
+        /// <summary>
+        /// 出队
+        /// </summary>
+        /// <param name="QKey"></param>
+        /// <returns></returns>
+        public string DeQueue(string QKey)
+        {
+            //1 编码字符串
+            byte[] bytes = redisClient.RPop(QKey);
+            string QMessage = null;
+            //2 redis消息队列出队
+            if (bytes == null)
+            {
+                Console.WriteLine("队列中的数据为空");
+            }
+            else
+            {
+                QMessage = System.Text.Encoding.UTF8.GetString(bytes);
+            }
+            return QMessage;
+        }
+
         public void Dispose()
         {
             throw new NotImplementedException();
