140620b30528dde1ee21f8ee3532ecfacea51743
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 16:48:46 +0800

    redis 消息队列

---
+++ MyRedisMq/MQ/RedisMessageQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRedisMq.MQ
+{
+    /// <summary>
+    /// redis 消息队列
+    /// </summary>
+    public class RedisMessageQueue : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
