65eae9ecba7cb0e009f813869d1a5a753dcbb0d8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 16:38:11 +0800

    订单积分服务

--- MyRedisMq/Async/RmOrderPoints.cs
+++ MyRedisMq/Async/RmOrderPoints.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace MyRedisMq.Async
 {
-    internal class RmOrderPoints
+    /// <summary>
+    /// 订单积分服务
+    /// </summary>
+    public class RmOrderPoints
     {
+        /// <summary>
+        /// 根据订单增加积分
+        /// </summary>
+        /// <param name="orderNo"></param>
+        public void AddPoints(string orderNo)
+        {
+            //1 调用积分服务
+            Thread.Sleep(1000);
+
+            //2 发送积分成功
+            Console.WriteLine($"增加积分：orderNo:{orderNo}成功");
+        }
+
     }
 }
