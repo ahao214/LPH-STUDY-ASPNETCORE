6a4ba8586395010a86f38d1ccc8958bbc1cfcf42
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 18:35:52 +0800

    asp.net中如何集成邮件发送和确认

--- EmailSendTest/obj/Debug/net6.0/EmailSendTest.AssemblyInfo.cs
+++ EmailSendTest/obj/Debug/net6.0/EmailSendTest.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-EmailSendTest-3AC14172-373A-404A-B8D1-F46AB13F2960")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmailSendTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
