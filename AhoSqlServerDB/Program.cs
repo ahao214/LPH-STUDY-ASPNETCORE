bbe1e700814e2100fa6576b7f68e23bb9bba89f5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 21:05:29 +0800

    多结果查询

--- AhoSqlServerDB/Program.cs
+++ AhoSqlServerDB/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-
+using System.Data;
 
 namespace AhoSqlServerDB
 {
@@ -16,17 +16,42 @@ namespace AhoSqlServerDB
 
         static void Main(string[] args)
         {
-            // 准备好SQL语句
-            string sql = "select ClassName from StudentClass where ClassName=N'网络班'";
+            #region 单一查询结果
+            //string sql = "select ClassName from StudentClass where ClassName=N'网络班'";
+
+            //SqlConnection conn = new SqlConnection(connString); // 链接数据库
+            //SqlCommand cmd = new SqlCommand(sql, conn); //发送SQL命令
+            //conn.Open();
+            //object result = cmd.ExecuteScalar();
+            //conn.Close();
+
+            //// 展示数据
+            //WriteLine($"班级名称是：{result.ToString()}");
+
+            #endregion
+
+            #region 多结果查询
+
+            string sql = "select Id,ClassName from StudentClass where Id=1";
 
             SqlConnection conn = new SqlConnection(connString); // 链接数据库
             SqlCommand cmd = new SqlCommand(sql, conn); //发送SQL命令
             conn.Open();
-            object result = cmd.ExecuteScalar();
-            conn.Close();
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            StudentClass studentClass = new StudentClass();
+            // 解析数据
+            if (reader.Read())
+            {
+                studentClass = new StudentClass()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    ClassName = reader["ClassName"].ToString()
+                };
+            }
 
-            // 展示数据
-            WriteLine($"班级名称是：{result.ToString()}");
+            WriteLine($"班级ID：{studentClass.Id},班级名称：{studentClass.ClassName}");
+
+            #endregion
 
             ReadKey();
 
