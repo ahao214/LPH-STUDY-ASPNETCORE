5084a803f1ac515ede7d51d739fbb17a2c6265ad
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 11:42:42 +0800

    商品MongoDB配置选项

--- EBusiness.Service/Models/ProductMongoDBOptions.cs
+++ EBusiness.Service/Models/ProductMongoDBOptions.cs
@@ -1,6 +1,22 @@
 ﻿namespace EBusiness.Service.Models
 {
+    /// <summary>
+    /// 商品MongoDB配置选项
+    /// </summary>
     public class ProductMongoDBOptions
     {
+        /// <summary>
+        /// 商品集合名
+        /// </summary>
+        public string ProductCollectionName { get; set; }
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public string CollectionString { get; set; }
+        /// <summary>
+        /// 商品数据库名
+        /// </summary>
+        public string DatabaseName { get; set; }
+
     }
 }
