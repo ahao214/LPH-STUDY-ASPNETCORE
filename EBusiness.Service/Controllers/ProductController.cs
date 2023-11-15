a25aa25c4a8af6a4ad422d7cfa5e5d5136f4619f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 11:14:42 +0800

    商品控制器

---
+++ EBusiness.Service/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EBusiness.Service.Controllers
+{
+    /// <summary>
+    /// 商品控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
--- EBusiness.Service/Services/ProductService.cs
+++ EBusiness.Service/Services/ProductService.cs
@@ -1,6 +1,58 @@
-﻿namespace EBusiness.Service.Services
+﻿using EBusiness.Service.Models;
+using System.Collections.Generic;
+using EBusiness.Service.Models;
+using MongoDB.Driver;
+
+namespace EBusiness.Service.Services
 {
-    public class ProductService
+    /// <summary>
+    /// 商品服务
+    /// </summary>
+    public class ProductService : IProductService
     {
+        private readonly IMongoCollection<Product> _products;
+
+
+        public ProductService()
+        {
+            //1 建立MongoDB连接
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            //2 获取商品库
+            var database = client.GetDatabase("productdb");
+
+            //3 获取商品表(集合)
+            _products = database.GetCollection<Product>("Product");
+        }
+
+        public void Create(Product product)
+        {
+            _products.InsertOne(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _products.DeleteOne(p => p.Id == product.Id);
+        }
+
+        public Product GetProductById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ProductExists(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
