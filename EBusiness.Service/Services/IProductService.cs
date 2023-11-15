19948e4902602aa8b9ce13c0862ab8d588d942ba
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 11:08:16 +0800

    商品服务接口

--- EBusiness.Service/Services/IProductService.cs
+++ EBusiness.Service/Services/IProductService.cs
@@ -1,6 +1,20 @@
-﻿namespace EBusiness.Service.Services
+﻿using EBusiness.Service.Models;
+using System.Collections.Generic;
+
+namespace EBusiness.Service.Services
 {
+    /// <summary>
+    /// 商品服务接口
+    /// </summary>
     public interface IProductService
     {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int id);
+        void Create(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        bool ProductExists(int id);
+
+
     }
 }
