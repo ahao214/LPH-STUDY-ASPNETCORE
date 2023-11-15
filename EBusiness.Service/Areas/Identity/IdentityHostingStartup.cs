dc0e2789145c0261f3f4edab37424f73ca775fc5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 19:42:27 +0800

    asp.net中如何集成邮件发送和确认

---
+++ EBusiness.Service/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EBusiness.Service.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EBusiness.Service.Areas.Identity.IdentityHostingStartup))]
+namespace EBusiness.Service.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EBusinessServiceContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EBusinessServiceContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EBusinessServiceContext>();
+            });
+        }
+    }
+}
