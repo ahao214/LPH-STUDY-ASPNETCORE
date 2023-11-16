6a5f68938d2d7421ef9ace4a32129bc2dc98c0bf
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-16 20:30:46 +0800

    方法

--- AhoSqlServerDB/Program.cs
+++ AhoSqlServerDB/Program.cs
@@ -11,11 +11,43 @@ namespace AhoSqlServerDB
 {
     public class Program
     {
-       
-
         static void Main(string[] args)
         {
-           
+            //string sql = "insert into StudentClass(ClassName) values(N'JS高级')";
+            //int result = SQLHelper.GetSingle(sql);
+            //if (result > 0)
+            //{
+            //    WriteLine("插入成功");
+            //}
+            //else
+            //{
+            //    WriteLine("插入失败");
+            //}
+
+
+            //string sql = "select id,classname from studentclass where id=10000";
+            //object stuObject = SQLHelper.GetSingleObject(sql);
+            //int classId = (int)stuObject;
+            //WriteLine($"班级ID:{classId}");
+
+            string sql = "select id,classname from studentclass";
+            SqlDataReader reader = SQLHelper.GetDataReader(sql);
+            List<StudentClass> stuList = new List<StudentClass>();
+
+            while (reader.Read())
+            {
+                StudentClass stu = new StudentClass();
+                stu.Id = Convert.ToInt32(reader["id"]);
+                stu.ClassName = reader["ClassName"].ToString();
+                stuList.Add(stu);
+            }
+
+            foreach (var item in stuList)
+            {
+                WriteLine($"班级ID:{item.Id},班级名称：{item.ClassName}");
+            }
+
+
 
             ReadKey();
 
