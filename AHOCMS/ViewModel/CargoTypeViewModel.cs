8c99e99d71dad1d0da89f510c6b6f8e67963cf15
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-16 15:06:35 +0800

    所有物资类型

--- AHOCMS/ViewModel/CargoTypeViewModel.cs
+++ AHOCMS/ViewModel/CargoTypeViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using AHOCMS.Models;
 
 namespace AHOCMS.ViewModel
 {
-    public class CargoTypeViewModel
+    public class CargoTypeViewModel:ViewModelBase
     {
+        private ObservableCollection<CargoType> cargoTypes = new ObservableCollection<CargoType>();
+
+        /// <summary>
+        /// 所有物资类型
+        /// </summary>
+        public ObservableCollection<CargoType> CargoTypes
+        {
+            get
+            {
+                return cargoTypes;
+            }
+            set
+            {
+                cargoTypes = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
