f7116bc113263558bb4c666eb6c8b43f311fe21e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-17 11:56:02 +0800

    泛型类

--- Ant-Generic/Program.cs
+++ Ant-Generic/Program.cs
@@ -11,7 +11,13 @@ Show<int>(111);
 Show<double>(2.3);
 Show<string>("block");
 
+ListBlock<int> lstBlock = new ListBlock<int>();
+lstBlock.Add(1);
+lstBlock.Add(2);
 
+ListBlock<string> strBlock = new ListBlock<string>();
+strBlock.Add("kuteng");
+strBlock.Add("block");
 
 #region 泛型方法
 
@@ -22,5 +28,20 @@ static void Show<T>(T param)
 
 
 
+
+#endregion
+
+
+#region 泛型类
+
+public class ListBlock<T>
+{
+    private ListBlock<T> _list;
+    public void Add(T item)
+    {
+        _list.Add(item);
+    }
+
+}
 
 #endregion
