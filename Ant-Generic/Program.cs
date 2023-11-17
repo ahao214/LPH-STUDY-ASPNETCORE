3da29370d06ece88437e30eb6dd376bbc49beee3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-17 11:46:47 +0800

    泛型

---
+++ Ant-Generic/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Title = "泛型";
+
+Show<int>(111);
+Show<double>(2.3);
+Show<string>("block");
+
+
+#region 泛型方法
+
+static void Show<T>(T param)
+{
+    WriteLine(param.GetType().Name + " " + param);
+}
+
+
+
+
+#endregion
