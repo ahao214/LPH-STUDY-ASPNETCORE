b8b75013f099000171f401e866685e09990e3f9e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-17 14:28:20 +0800

    委托

--- DelegateAndLambda/Program.cs
+++ DelegateAndLambda/Program.cs
@@ -13,6 +13,41 @@ namespace DelegateAndLambda
     {
         static void Main(string[] args)
         {
+            // 委托初始化
+            HelloDelegate helloDelegate = new HelloDelegate(Hello);
+            // 调用
+            helloDelegate.Invoke("");
+
+            AddDele addDele = new AddDele(Add);
+            int sum = addDele.Invoke(1, 2);
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+
+        public static void Hello(string msg)
+        {
+            Console.WriteLine("我是委托");
+        }
+
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+
+    //定义委托
+    delegate void HelloDelegate(string msg);
+
+    delegate int AddDele(int a, int b);
+
+    class MyClass
+    {
+        public void Hello(string msg)
+        {
+            Console.WriteLine("我是委托");
         }
     }
 }
