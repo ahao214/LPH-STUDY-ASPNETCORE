55e841f2edeb8522b353ce5edb1d9eed394cec05
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-17 13:40:04 +0800

    委托

---
+++ DelegateAndLambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndLambda
+{
+    /// <summary>
+    /// 委托
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
