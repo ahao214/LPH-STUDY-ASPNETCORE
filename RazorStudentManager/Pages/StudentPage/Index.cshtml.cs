5842a3c695d2f6945e3083b0141dbb167501e4cb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-18 20:11:50 +0800

    学生列表页面

--- RazorStudentManager/Pages/StudentPage/Index.cshtml.cs
+++ RazorStudentManager/Pages/StudentPage/Index.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorStudentManager.AppData;
+using RazorStudentManager.Model;
+using System.Collections.Generic;
 
 namespace RazorStudentManager.Pages.StudentPage
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+
+        private readonly AppDbContext _db;
+        public IndexModel(AppDbContext db)
         {
+            _db = db;
         }
+
+        public IEnumerable<Student> Students { get; set; }
+
+        public async void OnGet()
+        {
+            Students = await _db.Students.ToListAsync();
+        }
+
+
+
     }
 }
