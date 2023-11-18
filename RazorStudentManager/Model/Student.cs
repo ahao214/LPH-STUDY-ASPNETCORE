6dea584809eda835c1094b7bff1c3b63b6450540
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-18 18:23:07 +0800

    学生信息实体类

--- RazorStudentManager/Model/Student.cs
+++ RazorStudentManager/Model/Student.cs
@@ -7,7 +7,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
 
     }
 }
