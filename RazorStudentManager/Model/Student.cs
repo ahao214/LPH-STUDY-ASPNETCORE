67db81d5b03d29fa1a66cbce7a025908154840c0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-18 16:55:07 +0800

    添加学生实体类

---
+++ RazorStudentManager/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace RazorStudentManager.Model
+{
+    /// <summary>
+    /// 学生信息实体类
+    /// </summary>
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
