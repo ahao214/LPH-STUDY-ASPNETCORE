ebb0578a6b34d9adf86d7b0c73086f56fc81acf3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-18 20:41:06 +0800

    编辑数据方法

---
+++ RazorStudentManager/Pages/StudentPage/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorStudentManager.Model;
+using RazorStudentManager.AppData;
+using System.Threading.Tasks;
+
+namespace RazorStudentManager.Pages.StudentPage
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _db;
+        public EditModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]  //和前端的代码绑定
+        public Student student { get; set; }
+        public async void OnGet(int id)
+        {
+            student = await _db.Students.FindAsync(id);
+        }
+
+        /// <summary>
+        /// 编辑数据方法
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState .IsValid )
+            {
+                var studentObj = await _db.Students.FindAsync(student.Id);
+                studentObj.Name = student.Name;
+                studentObj.Age = student.Age;
+                studentObj.Phone = student.Phone;
+                studentObj.Address = student.Address;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+
+    }
+}
