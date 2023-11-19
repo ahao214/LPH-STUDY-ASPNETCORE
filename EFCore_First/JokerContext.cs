33ae287c7a5b41412968850fea258760fe74de8f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-19 20:52:26 +0800

    DbContext

---
+++ EFCore_First/JokerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EFCore_First
+{
+    /// <summary>
+    /// 数据基础类
+    /// </summary>
+    public class JokerContext : DbContext
+    {
+
+
+    }
+}
