e102be5813eb0708b875c7b81728ab3a93701f14
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-19 18:55:59 +0800

    IConfiguration

--- BookMVC/Startup.cs
+++ BookMVC/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,14 @@ namespace BookMVC
 {
     public class Startup
     {
+
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
