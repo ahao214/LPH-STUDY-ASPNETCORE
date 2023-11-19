17e662a4f5e82fa83d585aa4a661bab6b1696c2e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-19 16:44:21 +0800

    StudentClass

---
+++ RazorStudentManager/Model/StudentClass.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorStudentManager.Model
+{
+    /// <summary>
+    /// 班级表实体类
+    /// </summary>
+    public class StudentClass
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "班级名称")]
+        public string ClassName { get; set; }
+
+
+    }
+
+}
