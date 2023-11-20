627a7a6482454deb0251bd31cba92daab7728021
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-20 22:20:15 +0800

    封装验证码

---
+++ JokerBooksManagerComm/Comm/VerifyCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerComm.Comm
+{
+    /// <summary>
+    /// 封装验证码
+    /// </summary>
+    public  class VerifyCode
+    {
+
+
+    }
+}
