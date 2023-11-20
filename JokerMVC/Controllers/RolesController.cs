06b807196a88b0f921e721b9b04e72a93fe39307
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-20 21:07:32 +0800

    基础

--- JokerMVC/Controllers/RolesController.cs
+++ JokerMVC/Controllers/RolesController.cs
@@ -1,5 +1,7 @@
 ﻿using JokerMVC.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace JokerMVC.Controllers
 {
@@ -9,9 +11,13 @@ namespace JokerMVC.Controllers
     public class RolesController : Controller
     {
         private readonly ApplicationDbContext db;
-        public RolesController(ApplicationDbContext db)
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        public RolesController(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             this.db = db;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
         }
 
         public IActionResult Index()
