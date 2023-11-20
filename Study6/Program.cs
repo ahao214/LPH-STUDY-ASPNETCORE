0b0073f71bf6db26c61cf7007a4cc01224dbde77
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-20 21:26:05 +0800

    案例

--- Study6/Program.cs
+++ Study6/Program.cs
@@ -11,5 +11,21 @@ namespace Study6
         static void Main(string[] args)
         {
         }
+
+
+        private void CfkJob()
+        {
+            for(int i =1;i<10;i++)
+            {
+                for(int j = 1;j<=i;j++)
+                {
+                    int m = i * j;
+                    Console.Write($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
     }
 }
