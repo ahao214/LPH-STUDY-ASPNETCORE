cd58104c897a9ea0996355151cb38605cd66b45d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-20 22:25:14 +0800

    验证码字符串

---
+++ JokerBooksManagerComm/Comm/CommConst.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerComm.Comm
+{
+    /// <summary>
+    /// 抽象常量类
+    /// </summary>
+    public abstract class CommConst
+    {
+        #region 验证码字符串
+
+        /// <summary>
+        /// 验证码字符串
+        /// </summary>
+        protected const string AllChar = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+
+        #endregion
+    }
+}
