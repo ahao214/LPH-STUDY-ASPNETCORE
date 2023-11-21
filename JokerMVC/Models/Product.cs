d84abae8cd3f0a1745794e524a10d2adc2009780
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 14:53:35 +0800

    商品名称显示

--- JokerMVC/Models/Product.cs
+++ JokerMVC/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JokerMVC.Models
@@ -8,10 +9,15 @@ namespace JokerMVC.Models
     /// </summary>
     public class Product
     {
+        [Display(Name = "商品ID")]
         public int Id { get; set; }
+        [Display(Name = "商品名称")]
         public string Name { get; set; }
+        [Display(Name = "商品价格")]
         public double Price { get; set; }
+        [Display(Name = "商品库存")]
         public short? Stock { get; set; }
+        [Display(Name = "商品图片")]
         public string? ImageUrl { get; set; }
         [NotMapped]
         public IFormFile? FormFile { get; set; }
