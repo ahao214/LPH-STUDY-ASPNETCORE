a9afdb575220199a93fd145b2566429a809bbf40
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 10:12:38 +0800

    获取MD5哈希值

---
+++ JokerBooksManagerComm/Comm/CommDefine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace JokerBooksManagerComm.Comm
+{
+    public class CommDefine
+    {
+
+        #region 获取MD5哈希值
+
+        /// <summary>
+        /// 获取MD5哈希值
+        /// </summary>
+        /// <param name="sVal">传入的字符</param>
+        /// <returns>返回加密后的字符</returns>
+        public static string GetMD5Hash(string sVal)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] bs = md5.ComputeHash(Encoding.Default.GetBytes(sVal));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bs.Length; i++)
+            {
+                sb.Append(bs[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        #endregion
+
+    }
+}
