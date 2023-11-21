2200d22fbe7583b65d237cc92c1e8d341d9d3bc3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 15:18:02 +0800

    商品

--- JokerMVC/Controllers/ProductsController.cs
+++ JokerMVC/Controllers/ProductsController.cs
@@ -111,9 +111,7 @@ namespace JokerMVC.Controllers
             return View(product);
         }
 
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Stock,ImageUrl")] Product product)
