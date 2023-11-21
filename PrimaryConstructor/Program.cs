4f014028eb5294a0b264830d5886e01e790e14ec
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 23:44:41 +0800

    主要构造函数

---
+++ PrimaryConstructor/Program.cs
@@ -0,0 +1,12 @@
+﻿// 主要构造函数
+
+
+class MyClass(string? name = default)
+{
+    public MyClass(string? name, int? Age = default) : this(name)
+    {
+        Age = Age;
+    }
+    public string? Name => name;
+    public int? Age { get; }
+}
