be1c628cadc1cc1738c23317dbf094aad377bf12
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 19:05:04 +0800

    redis

---
+++ T4/Program.cs
@@ -0,0 +1,51 @@
+﻿using static System.Console;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+
+
+
+var redisDB = ConnectionMultiplexer.Connect("127.0.0.1:6379").GetDatabase(0);
+
+Product p = new Product()
+{
+    Id = 1,
+    Name = "笔记本",
+    Price = 9999,
+    Stock = 100
+};
+
+// 销售数量
+redisDB.StringSet("saleNum", 0);
+int saleNum = Convert.ToInt32(redisDB.StringGet("saleNum"));
+
+// 1 创建事务
+var tran = redisDB.CreateTransaction();
+// 2 添加事务约束(监视字段)加锁
+tran.AddCondition(Condition.StringEqual("saleNum", saleNum));
+while (saleNum < p.Stock)
+{
+    saleNum = new Random().Next(1, 10);
+    redisDB.StringSetAsync("saleNum", saleNum);
+    // 保存用户信息
+    string id = Guid.NewGuid().ToString();
+    string customer = $"用户：{id}";
+
+    redisDB.SetAddAsync("customers", customer);
+
+}
+
+// 3 执行(提交)事务
+
+
+
+
+
+class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Stock { get; set; }
+}
--- T4/obj/Debug/net6.0/T4.AssemblyInfo.cs
+++ T4/obj/Debug/net6.0/T4.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("T3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("T4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("T3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("T3")]
+[assembly: System.Reflection.AssemblyProductAttribute("T4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("T4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
