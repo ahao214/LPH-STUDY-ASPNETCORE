63651292790f9543f9aafa0e5244d967d32111f5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 13:44:39 +0800

    商品实体类

---
+++ JokerMVC/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿
+namespace JokerMVC.Models
+{
+    /// <summary>
+    /// 商品实体类
+    /// </summary>
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public short? Stock { get; set; }
+        public string? ImageUrl { get; set; }
+        public IFormFile? FormFile { get; set; }
+
+    }
+}
