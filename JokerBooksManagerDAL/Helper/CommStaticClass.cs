5d60044795afc84dcc015db76b7dc4df50b4bafb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 21:44:02 +0800

    DAL 层

---
+++ JokerBooksManagerDAL/Helper/CommStaticClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerDAL.Helper
+{
+    /// <summary>
+    /// 定义抽象类存放连接字符串
+    /// </summary>
+    public abstract class CommStaticClass
+    {
+        #region 连接字符串
+
+        public static string ConnectionStrings;
+
+        #endregion
+
+    }
+}
---
+++ JokerBooksManagerDAL/Helper/DBHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+
+namespace JokerBooksManagerDAL.Helper
+{
+    /// <summary>
+    /// DBHelper类用来封装数据库操作事情
+    /// </summary>
+    public class DBHelper: CommStaticClass
+    {
+        #region 创建数据库链接对象
+
+
+
+        #endregion
+
+
+    }
+}
