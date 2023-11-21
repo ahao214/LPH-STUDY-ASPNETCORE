c7f85cf8d0403041ebc8461b1ca39371fb0ea5b6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 22:05:08 +0800

    新建文档

---
+++ JokerBooksManagerDAL/BookDAL/ManagerDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace JokerBooksManagerDAL.BookDAL
+{
+    /// <summary>
+    /// 数据库访问层管理员类
+    /// </summary>
+    public class ManagerDAL
+    {
+
+
+    }
+}
