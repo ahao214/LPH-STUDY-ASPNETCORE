b2950330329000fa0e100efc6f42c0f3bed2a833
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-22 14:49:07 +0800

    新增读者类别

---
+++ JokerBooksManagerBLL/BookBLL/ReaderTypeBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JokerBooksManagerModels.Model;
+using JokerBooksManagerDAL.BookDAL;
+using JokerBooksManagerDAL.Helper;
+using System.Data.SqlClient;
+
+
+namespace JokerBooksManagerBLL.BookBLL
+{
+    public class ReaderTypeBLL
+    {
+        #region 新增读者类别
+
+        /// <summary>
+        /// 新增读者类别
+        /// </summary>
+        /// <param name="readerType">读者类别</param>
+        /// <returns>大于0：成功 小于0：失败</returns>
+        public int AddReaderType(ReaderType readerType)
+        {
+            try
+            {
+                return ReaderTypeDAL.AddReaderType(readerType);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ JokerBooksManagerDAL/BookDAL/ReaderTypeDAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using JokerBooksManagerComm.Comm;
+using JokerBooksManagerDAL.Helper;
+using JokerBooksManagerModels.Model;
+
+
+namespace JokerBooksManagerDAL.BookDAL
+{
+    /// <summary>
+    /// 数据库访问层读者类别
+    /// </summary>
+    public class ReaderTypeDAL
+    {
+        #region 新增读者类别
+
+        /// <summary>
+        /// 新增读者类别
+        /// </summary>
+        /// <param name="readerType">读者类别</param>
+        /// <returns>大于0：成功 小于0：失败</returns>
+        public static int AddReaderType(ReaderType readerType)
+        {
+            StringBuilder sb = new StringBuilder();
+            BookCommandType commandType = BookCommandType.Text;
+            sb.Append("INSERT INTO ReaderType(ReadTypeName)VALUES(@ReadTypeName)");
+            SqlParameter[] paras =
+            {
+                new SqlParameter ("@ReadTypeName",readerType .ReadTypeName)
+            };
+            try
+            {
+                return DBHelper.ExecuteNoneQuery(sb.ToString(), commandType, paras);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+    }
+}
