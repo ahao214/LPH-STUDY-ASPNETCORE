bb3f4b9f59251c51d7a6bc9bfa1b6c0b09f08fca
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-23 23:53:00 +0800

    MVVM 建立通信

---
+++ TestEventAggregator/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+
+
+namespace TestEventAggregator
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged(string propName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void RaisePropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            MemberExpression memberExpression =(MemberExpression) expression.Body;
+            string propName = memberExpression .Member.Name;
+            RaisePropertyChanged(propName);
+        }
+    }
+}
