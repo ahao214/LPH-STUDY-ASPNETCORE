dbf13b536407a5b3dc14d32950cd28f43b89402e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-23 21:30:44 +0800

    add 转换文件

---
+++ DataBoard/Converter/HeightToCornerConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Converter
+{
+    /// <summary>
+    /// 将高度值分成一半
+    /// </summary>
+    public class HeightToCornerConverter
+    {
+
+
+    }
+}
