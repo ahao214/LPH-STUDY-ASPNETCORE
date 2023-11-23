032587cb1fdc9481ec6e7998c78e13fef94741e9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-23 15:34:21 +0800

    出入库

---
+++ AHOCMS/ViewModel/InputCargoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHOCMS.ViewModel
+{
+    /// <summary>
+    /// 入库管理
+    /// </summary>
+    public class InputCargoViewModel: ViewModelBase
+    {
+    }
+}
