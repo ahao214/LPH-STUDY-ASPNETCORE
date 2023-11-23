991c4a8f8700582b6fbaf9936d9d5828ac4e2d26
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-23 21:50:28 +0800

    HeightToCornerConverter

--- DataBoard/Converter/HeightToCornerConverter.cs
+++ DataBoard/Converter/HeightToCornerConverter.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+
 
 namespace DataBoard.Converter
 {
     /// <summary>
     /// 将高度值分成一半
     /// </summary>
-    public class HeightToCornerConverter
+    public class HeightToCornerConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return value;
+            if (double.TryParse(value.ToString(), out double height))
+                return height / 2;
+            else
+                return value;
+        }
 
-
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
