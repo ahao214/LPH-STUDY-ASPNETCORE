adf8cb95ccbbc38dc7201a74cd179814b3a1f09c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-23 15:37:58 +0800

    出入库流水单

--- AHOCMS/ViewModel/InputCargoViewModel.cs
+++ AHOCMS/ViewModel/InputCargoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AHOCMS.Models;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,24 @@ namespace AHOCMS.ViewModel
     /// </summary>
     public class InputCargoViewModel: ViewModelBase
     {
+        public AppData AppData { get; set; } = new AppData();
+
+        private Record record = new Record();
+
+        /// <summary>
+        /// 入库流水单
+        /// </summary>
+        public Record Record
+        {
+            get
+            {
+                return record;
+            }
+            set
+            {
+                record = value; RaisePropertyChanged();
+            }
+        }
+
     }
 }
--- AHOCMS/ViewModel/OutputCargoViewModel.cs
+++ AHOCMS/ViewModel/OutputCargoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using AHOCMS.Models;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,23 @@ namespace AHOCMS.ViewModel
     /// </summary>
     public class OutputCargoViewModel: ViewModelBase
     {
+        public AppData AppData { get; set; } = new AppData();
+
+        private Record record = new Record();
+
+        /// <summary>
+        /// 出库流水单
+        /// </summary>
+        public Record Record
+        {
+            get
+            {
+                return record;
+            }
+            set
+            {
+                record = value; RaisePropertyChanged();
+            }
+        }
     }
 }
