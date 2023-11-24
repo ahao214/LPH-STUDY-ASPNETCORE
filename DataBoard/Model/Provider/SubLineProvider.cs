75573e9a702e402887c6377223275c2175210669
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:14:55 +0800

    子线类型模型方法

---
+++ DataBoard/Model/Provider/SubLineProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Model.Provider
+{
+    /// <summary>
+    /// 子线类型模型方法
+    /// </summary>
+    public class SubLineProvider : IProvider<SubLine>
+    {
+        public int Delete(SubLine t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(SubLine t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SubLine> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(SubLine t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
