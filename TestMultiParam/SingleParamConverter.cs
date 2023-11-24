0fc94acd4a930956151a02050ec86e11235238c6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 13:58:51 +0800

    Button.Style

---
+++ TestMultiParam/SingleParamConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+
+namespace TestMultiParam
+{
+    public class SingleParamConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return true;
+            }
+            int age = int.Parse(value.ToString());
+            if (age > 23)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
