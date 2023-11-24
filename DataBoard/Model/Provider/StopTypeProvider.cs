b780a747576669c872645a17ae08d17e63ccc4a2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:15:31 +0800

    停机类型模型方法

---
+++ DataBoard/Model/Provider/StopTypeProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Model.Provider
+{
+    /// <summary>
+    /// 停机类型模型方法
+    /// </summary>
+    public class StopTypeProvider : IProvider<StopType>
+    {
+        public int Delete(StopType t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(StopType t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StopType> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(StopType t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
