9cd460a0096b2a6231508b8ca1e4a1f14a992d87
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 14:05:17 +0800

    datagrid

---
+++ TestMultiParam/MultiParamConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+namespace TestMultiParam
+{
+    public class MultiParamConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null)
+                return true;
+            int age = int.Parse(values[0].ToString());
+            int grade = int.Parse(values[1].ToString());
+            if (age > 24 && grade > 7)
+                return true;
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
