6ff8353adfe631674ab308149dc1f3742d50f995
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:09:46 +0800

    方法接口

---
+++ DataBoard/Model/Provider/IProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Model.Provider
+{
+    /// <summary>
+    /// 方法接口
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IProvider<T> where T : class
+    {
+        List<T> Select();
+        int Insert(T t);
+        int Update (T t);
+        int Delete(T t);
+    }
+}
--- DataBoard/ViewModel/LineViewModel.cs
+++ DataBoard/ViewModel/LineViewModel.cs
@@ -1,13 +1,35 @@
-﻿using GalaSoft.MvvmLight;
+﻿using DataBoard.Model;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBoard.ViewModel
 {
-    public class LineViewModel:ViewModelBase
+    /// <summary>
+    /// 生产线
+    /// </summary>
+    public class LineViewModel : ViewModelBase
     {
+        private List<Line> lines;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public LineViewModel()
+        {
+                
+        }
+
+        public List<Line> Lines
+        {
+            get { return lines; }
+            set { lines = value; }
+        }
+
+       
+
     }
 }
