5bc70dd653a54f38c2c8f28a3b4bdc2dd324dc77
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:16:17 +0800

    停机数据类型模型方法

---
+++ DataBoard/Model/Provider/HistoryProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Model.Provider
+{
+    /// <summary>
+    /// 停机数据类型模型方法  
+    /// </summary>
+    public class HistoryProvider : IProvider<History>
+    {
+        public int Delete(History t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(History t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<History> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(History t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
