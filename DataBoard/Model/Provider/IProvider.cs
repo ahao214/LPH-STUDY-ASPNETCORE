771b9d824484be8bf4b4cd8f0994466c68e00365
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:10:28 +0800

    方法接口

--- DataBoard/Model/Provider/IProvider.cs
+++ DataBoard/Model/Provider/IProvider.cs
@@ -12,9 +12,28 @@ namespace DataBoard.Model.Provider
     /// <typeparam name="T"></typeparam>
     public interface IProvider<T> where T : class
     {
+        /// <summary>
+        /// 返回所有数据
+        /// </summary>
+        /// <returns></returns>
         List<T> Select();
+        /// <summary>
+        /// 插入数据
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         int Insert(T t);
+        /// <summary>
+        /// 编辑数据
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         int Update (T t);
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         int Delete(T t);
     }
 }
