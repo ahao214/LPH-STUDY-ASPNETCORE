6e840d2b39eed9dcec0b8a73e4ea1c5f88596396
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:13:59 +0800

    用户模型方法

---
+++ DataBoard/Model/Provider/UserInfoProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Model.Provider
+{
+    /// <summary>
+    /// 用户模型方法
+    /// </summary>
+    public class UserInfoProvider : IProvider<UserInfo>
+    {
+        public int Delete(UserInfo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(UserInfo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserInfo> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(UserInfo t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
