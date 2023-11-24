7a9aef82830e9f50cd763b00af2df67e457a9363
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 16:25:58 +0800

    MVC Project

--- TestMultiParam/obj/Debug/net6.0-windows/TestMultiParam.AssemblyInfo.cs
+++ TestMultiParam/obj/Debug/net6.0-windows/TestMultiParam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestMultiParam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc94acd4a930956151a02050ec86e11235238c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestMultiParam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestMultiParam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
