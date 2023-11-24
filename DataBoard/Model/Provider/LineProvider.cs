2262de727e0d359523248bcec937edbb3320960b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 15:11:56 +0800

    生产线模型方法

---
+++ DataBoard/Model/Provider/LineProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoard.Model.Provider
+{
+    /// <summary>
+    /// 生产线模型方法
+    /// </summary>
+    public class LineProvider : IProvider<Line>
+    {
+        public int Delete(Line t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Line t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Line> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Line t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
