2cc0d2baf8b077dba566f17044d48bdc5b09d917
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-25 22:38:51 +0800

    dd

---
+++ JokerBooksManagerDAL/BookDAL/BookTypeDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerDAL.BookDAL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BookTypeDAL
+    {
+    }
+}
