27dab90c5538091aa4d9093afcf61f8a192975c1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-25 22:33:02 +0800

    图书类别模型类

---
+++ JokerBooksManagerModels/Model/BookType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerModels.Model
+{
+    /// <summary>
+    /// 图书类别模型类
+    /// </summary>
+    public class BookType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Price { get; set; }
+
+
+
+
+
+
+    }
+}
