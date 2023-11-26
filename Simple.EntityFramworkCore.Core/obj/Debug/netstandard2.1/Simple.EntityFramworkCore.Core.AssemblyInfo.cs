f1f03b53c3fd2c63ce0f1079839fc7d98e450536
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-26 19:01:38 +0800

    用户表

--- Simple.EntityFramworkCore.Core/obj/Debug/netstandard2.1/Simple.EntityFramworkCore.Core.AssemblyInfo.cs
+++ Simple.EntityFramworkCore.Core/obj/Debug/netstandard2.1/Simple.EntityFramworkCore.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simple.EntityFramworkCore.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4537d8808af51682162faefdf7bc2dc016bf6d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simple.EntityFramworkCore.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simple.EntityFramworkCore.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Video.Domain/Users/UserInfo.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace Video.Domain.Users
+{
+    /// <summary>
+    /// 用户表
+    /// </summary>
+    public class UserInfo
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string? UserName { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string? Password { get; set; }
+        /// <summary>
+        /// 用户头像
+        /// </summary>
+        public string? Avatar { get; set; }
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool Enable { get; set; }
+
+
+
+    }
+}
