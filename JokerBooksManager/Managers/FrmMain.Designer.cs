c399c6661e44171a38333b184a91c5b71561ad47
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-26 16:50:33 +0800

    左侧菜单

--- JokerBooksManager/Managers/FrmMain.Designer.cs
+++ JokerBooksManager/Managers/FrmMain.Designer.cs
@@ -38,13 +38,14 @@
             this.TsmiReaderTypeName = new System.Windows.Forms.ToolStripMenuItem();
             this.TsmiReaderInfo = new System.Windows.Forms.ToolStripMenuItem();
             this.TsmiWindowList = new System.Windows.Forms.ToolStripMenuItem();
+            this.TsmiBookTypeManager = new System.Windows.Forms.ToolStripMenuItem();
+            this.TsmiBookTypeList = new System.Windows.Forms.ToolStripMenuItem();
             this.Sstatus = new System.Windows.Forms.StatusStrip();
             this.TsslTxtInfo = new System.Windows.Forms.ToolStripStatusLabel();
             this.TsslLoginName = new System.Windows.Forms.ToolStripStatusLabel();
             this.TsslTime = new System.Windows.Forms.ToolStripStatusLabel();
             this.Timers = new System.Windows.Forms.Timer(this.components);
-            this.TsmiBookTypeManager = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmiBookTypeList = new System.Windows.Forms.ToolStripMenuItem();
+            this.TvBookType = new Sunny.UI.UITreeView();
             this.MsMenu.SuspendLayout();
             this.Sstatus.SuspendLayout();
             this.SuspendLayout();
@@ -73,7 +74,7 @@
             // 
             this.TsmiPassModify.Image = global::JokerBooksManager.Properties.Resources._2;
             this.TsmiPassModify.Name = "TsmiPassModify";
-            this.TsmiPassModify.Size = new System.Drawing.Size(270, 34);
+            this.TsmiPassModify.Size = new System.Drawing.Size(209, 34);
             this.TsmiPassModify.Text = "密码修改(&P)";
             this.TsmiPassModify.Click += new System.EventHandler(this.TsmiPassModify_Click);
             // 
@@ -81,7 +82,7 @@
             // 
             this.TmsiExitSystem.Image = global::JokerBooksManager.Properties.Resources._3;
             this.TmsiExitSystem.Name = "TmsiExitSystem";
-            this.TmsiExitSystem.Size = new System.Drawing.Size(270, 34);
+            this.TmsiExitSystem.Size = new System.Drawing.Size(209, 34);
             this.TmsiExitSystem.Text = "退出系统(&Q)";
             this.TmsiExitSystem.Click += new System.EventHandler(this.TmsiExitSystem_Click);
             // 
@@ -114,14 +115,14 @@
             // TsmiReaderTypeName
             // 
             this.TsmiReaderTypeName.Name = "TsmiReaderTypeName";
-            this.TsmiReaderTypeName.Size = new System.Drawing.Size(270, 34);
+            this.TsmiReaderTypeName.Size = new System.Drawing.Size(204, 34);
             this.TsmiReaderTypeName.Text = "读者类别(&T)";
             this.TsmiReaderTypeName.Click += new System.EventHandler(this.TsmiReaderTypeName_Click);
             // 
             // TsmiReaderInfo
             // 
             this.TsmiReaderInfo.Name = "TsmiReaderInfo";
-            this.TsmiReaderInfo.Size = new System.Drawing.Size(270, 34);
+            this.TsmiReaderInfo.Size = new System.Drawing.Size(204, 34);
             this.TsmiReaderInfo.Text = "读者信息(&I)";
             this.TsmiReaderInfo.Click += new System.EventHandler(this.TsmiReaderInfo_Click);
             // 
@@ -132,6 +133,23 @@
             this.TsmiWindowList.Size = new System.Drawing.Size(122, 28);
             this.TsmiWindowList.Text = "窗口列表";
             // 
+            // TsmiBookTypeManager
+            // 
+            this.TsmiBookTypeManager.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TsmiBookTypeList});
+            this.TsmiBookTypeManager.Image = global::JokerBooksManager.Properties.Resources._1;
+            this.TsmiBookTypeManager.Name = "TsmiBookTypeManager";
+            this.TsmiBookTypeManager.Size = new System.Drawing.Size(122, 28);
+            this.TsmiBookTypeManager.Text = "图书类别";
+            // 
+            // TsmiBookTypeList
+            // 
+            this.TsmiBookTypeList.Image = global::JokerBooksManager.Properties.Resources._2;
+            this.TsmiBookTypeList.Name = "TsmiBookTypeList";
+            this.TsmiBookTypeList.Size = new System.Drawing.Size(218, 34);
+            this.TsmiBookTypeList.Text = "图书类别列表";
+            this.TsmiBookTypeList.Click += new System.EventHandler(this.TsmiBookTypeList_Click);
+            // 
             // Sstatus
             // 
             this.Sstatus.ImageScalingSize = new System.Drawing.Size(24, 24);
@@ -139,7 +157,7 @@
             this.TsslTxtInfo,
             this.TsslLoginName,
             this.TsslTime});
-            this.Sstatus.Location = new System.Drawing.Point(0, 657);
+            this.Sstatus.Location = new System.Drawing.Point(0, 989);
             this.Sstatus.Name = "Sstatus";
             this.Sstatus.Size = new System.Drawing.Size(1189, 31);
             this.Sstatus.TabIndex = 2;
@@ -172,28 +190,33 @@
             this.Timers.Interval = 1000;
             this.Timers.Tick += new System.EventHandler(this.Timers_Tick);
             // 
-            // TsmiBookTypeManager
+            // TvBookType
             // 
-            this.TsmiBookTypeManager.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.TsmiBookTypeList});
-            this.TsmiBookTypeManager.Image = global::JokerBooksManager.Properties.Resources._1;
-            this.TsmiBookTypeManager.Name = "TsmiBookTypeManager";
-            this.TsmiBookTypeManager.Size = new System.Drawing.Size(122, 28);
-            this.TsmiBookTypeManager.Text = "图书类别";
-            // 
-            // TsmiBookTypeList
-            // 
-            this.TsmiBookTypeList.Image = global::JokerBooksManager.Properties.Resources._2;
-            this.TsmiBookTypeList.Name = "TsmiBookTypeList";
-            this.TsmiBookTypeList.Size = new System.Drawing.Size(270, 34);
-            this.TsmiBookTypeList.Text = "图书类别列表";
-            this.TsmiBookTypeList.Click += new System.EventHandler(this.TsmiBookTypeList_Click);
+            this.TvBookType.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TvBookType.FillColor = System.Drawing.Color.White;
+            this.TvBookType.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.TvBookType.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TvBookType.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(242)))), ((int)(((byte)(212)))));
+            this.TvBookType.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.TvBookType.Location = new System.Drawing.Point(0, 91);
+            this.TvBookType.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TvBookType.MinimumSize = new System.Drawing.Size(1, 1);
+            this.TvBookType.Name = "TvBookType";
+            this.TvBookType.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.TvBookType.SelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.TvBookType.ShowText = false;
+            this.TvBookType.Size = new System.Drawing.Size(294, 898);
+            this.TvBookType.Style = Sunny.UI.UIStyle.Green;
+            this.TvBookType.TabIndex = 3;
+            this.TvBookType.Text = "uiTreeView1";
+            this.TvBookType.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(1189, 688);
+            this.ClientSize = new System.Drawing.Size(1189, 1020);
             this.ControlBoxFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.Controls.Add(this.TvBookType);
             this.Controls.Add(this.Sstatus);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.MsMenu);
@@ -234,5 +257,6 @@
         private System.Windows.Forms.ToolStripMenuItem TsmiReaderInfo;
         private System.Windows.Forms.ToolStripMenuItem TsmiBookTypeManager;
         private System.Windows.Forms.ToolStripMenuItem TsmiBookTypeList;
+        private Sunny.UI.UITreeView TvBookType;
     }
 }
