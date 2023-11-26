cb2acd75e5c7be5dd05051542a72d4785e3df155
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-26 17:08:02 +0800

    DbContext

--- Simple.EntityFramworkCore/Class1.cs
+++ Simple.EntityFramworkCore/Class1.cs
@@ -1,7 +1,30 @@
-﻿namespace Simple.EntityFramworkCore
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Simple.EntityFramworkCore
 {
-    public class Class1
+    public class MasterDbContext<IDbContext> : DbContext where IDbContext : DbContext
     {
+        public MasterDbContext(DbContextOptions<IDbContext> options) : base(options)
+        {
+
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 全局禁用跟踪查询
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution);
+
+
+#if DEBUG
+
+            // 显示更详细的异常日志
+            optionsBuilder.EnableDetailedErrors();
+
+#endif
+        }
+
 
     }
 }
