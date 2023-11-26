df9f48627f960c346755b86fa856b23ebe431fec
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-26 13:23:16 +0800

    添加图书类别窗体

---
+++ JokerBooksManager/Managers/FrmBookTypeAdd.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+
+namespace JokerBooksManager.Managers
+{
+    public partial class FrmBookTypeAdd : UIForm
+    {
+        public FrmBookTypeAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBookTypeAdd_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region 关闭窗体
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
