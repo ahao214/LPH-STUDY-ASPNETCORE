560fa3d97860874c34074d445412d9040ecf09a1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-26 13:17:04 +0800

    新建图书类别窗体

---
+++ JokerBooksManager/Managers/FrmBookTypeList.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace JokerBooksManager.Managers
+{
+    partial class FrmBookTypeList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DgvBookType = new Sunny.UI.UIDataGridView();
+            this.BtnClose = new Sunny.UI.UIButton();
+            this.BtnAddNew = new Sunny.UI.UIButton();
+            this.BookTypeId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookTypeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Remark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modify = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.DelData = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.PIBookType = new Sunny.UI.UIPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvBookType)).BeginInit();
+            this.PIBookType.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DgvBookType
+            // 
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.DgvBookType.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.DgvBookType.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.DgvBookType.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvBookType.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.DgvBookType.ColumnHeadersHeight = 32;
+            this.DgvBookType.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DgvBookType.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BookTypeId,
+            this.BookTypeName,
+            this.Remark,
+            this.Modify,
+            this.DelData});
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(242)))), ((int)(((byte)(212)))));
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvBookType.DefaultCellStyle = dataGridViewCellStyle8;
+            this.DgvBookType.EnableHeadersVisualStyles = false;
+            this.DgvBookType.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.DgvBookType.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(199)))), ((int)(((byte)(69)))));
+            this.DgvBookType.Location = new System.Drawing.Point(-2, 5);
+            this.DgvBookType.Name = "DgvBookType";
+            this.DgvBookType.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvBookType.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.DgvBookType.RowHeadersWidth = 62;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(242)))), ((int)(((byte)(212)))));
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.DgvBookType.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            this.DgvBookType.RowTemplate.Height = 30;
+            this.DgvBookType.ScrollBarBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.DgvBookType.ScrollBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.DgvBookType.ScrollBarRectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.DgvBookType.SelectedIndex = -1;
+            this.DgvBookType.Size = new System.Drawing.Size(926, 365);
+            this.DgvBookType.StripeOddColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.DgvBookType.Style = Sunny.UI.UIStyle.Green;
+            this.DgvBookType.TabIndex = 5;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.BtnClose.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.BtnClose.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.BtnClose.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnClose.FillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnClose.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnClose.Location = new System.Drawing.Point(820, 439);
+            this.BtnClose.MinimumSize = new System.Drawing.Size(1, 1);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.BtnClose.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.BtnClose.RectPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnClose.RectSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnClose.Size = new System.Drawing.Size(100, 35);
+            this.BtnClose.Style = Sunny.UI.UIStyle.Green;
+            this.BtnClose.TabIndex = 4;
+            this.BtnClose.Text = "关闭";
+            this.BtnClose.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // BtnAddNew
+            // 
+            this.BtnAddNew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAddNew.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.BtnAddNew.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.BtnAddNew.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.BtnAddNew.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnAddNew.FillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnAddNew.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnAddNew.Location = new System.Drawing.Point(653, 439);
+            this.BtnAddNew.MinimumSize = new System.Drawing.Size(1, 1);
+            this.BtnAddNew.Name = "BtnAddNew";
+            this.BtnAddNew.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.BtnAddNew.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.BtnAddNew.RectPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnAddNew.RectSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(152)))), ((int)(((byte)(32)))));
+            this.BtnAddNew.Size = new System.Drawing.Size(100, 35);
+            this.BtnAddNew.Style = Sunny.UI.UIStyle.Green;
+            this.BtnAddNew.TabIndex = 3;
+            this.BtnAddNew.Text = "新增类别";
+            this.BtnAddNew.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnAddNew.Click += new System.EventHandler(this.BtnAddNew_Click);
+            // 
+            // BookTypeId
+            // 
+            this.BookTypeId.DataPropertyName = "BookTypeId";
+            this.BookTypeId.HeaderText = "类别ID";
+            this.BookTypeId.MinimumWidth = 8;
+            this.BookTypeId.Name = "BookTypeId";
+            this.BookTypeId.ToolTipText = "类别ID";
+            this.BookTypeId.Width = 200;
+            // 
+            // BookTypeName
+            // 
+            this.BookTypeName.DataPropertyName = "BookTypeName";
+            this.BookTypeName.HeaderText = "图书类别";
+            this.BookTypeName.MinimumWidth = 8;
+            this.BookTypeName.Name = "BookTypeName";
+            this.BookTypeName.ToolTipText = "图书类别";
+            this.BookTypeName.Width = 200;
+            // 
+            // Remark
+            // 
+            this.Remark.DataPropertyName = "Remark";
+            this.Remark.HeaderText = "备注";
+            this.Remark.MinimumWidth = 8;
+            this.Remark.Name = "Remark";
+            this.Remark.ToolTipText = "备注";
+            this.Remark.Width = 150;
+            // 
+            // Modify
+            // 
+            this.Modify.HeaderText = "修改";
+            this.Modify.LinkColor = System.Drawing.Color.Lime;
+            this.Modify.MinimumWidth = 8;
+            this.Modify.Name = "Modify";
+            this.Modify.Text = "修改";
+            this.Modify.ToolTipText = "修改";
+            this.Modify.UseColumnTextForLinkValue = true;
+            this.Modify.Width = 150;
+            // 
+            // DelData
+            // 
+            this.DelData.HeaderText = "删除";
+            this.DelData.LinkColor = System.Drawing.Color.Lime;
+            this.DelData.MinimumWidth = 8;
+            this.DelData.Name = "DelData";
+            this.DelData.Text = "删除";
+            this.DelData.ToolTipText = "删除";
+            this.DelData.UseColumnTextForLinkValue = true;
+            this.DelData.Width = 150;
+            // 
+            // PIBookType
+            // 
+            this.PIBookType.Controls.Add(this.DgvBookType);
+            this.PIBookType.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.PIBookType.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(251)))), ((int)(((byte)(241)))));
+            this.PIBookType.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PIBookType.Location = new System.Drawing.Point(4, 40);
+            this.PIBookType.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PIBookType.MinimumSize = new System.Drawing.Size(1, 1);
+            this.PIBookType.Name = "PIBookType";
+            this.PIBookType.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.PIBookType.Size = new System.Drawing.Size(924, 373);
+            this.PIBookType.Style = Sunny.UI.UIStyle.Green;
+            this.PIBookType.TabIndex = 6;
+            this.PIBookType.Text = "uiPanel1";
+            this.PIBookType.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmBookTypeList
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(967, 527);
+            this.ControlBoxFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.Controls.Add(this.PIBookType);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnAddNew);
+            this.MaximizeBox = false;
+            this.Name = "FrmBookTypeList";
+            this.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Style = Sunny.UI.UIStyle.Green;
+            this.Text = "图书类别";
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.ZoomScaleRect = new System.Drawing.Rectangle(22, 22, 1258, 646);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvBookType)).EndInit();
+            this.PIBookType.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UIDataGridView DgvBookType;
+        private Sunny.UI.UIButton BtnClose;
+        private Sunny.UI.UIButton BtnAddNew;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookTypeId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookTypeName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Remark;
+        private System.Windows.Forms.DataGridViewLinkColumn Modify;
+        private System.Windows.Forms.DataGridViewLinkColumn DelData;
+        private Sunny.UI.UIPanel PIBookType;
+    }
+}
---
+++ JokerBooksManager/Managers/FrmBookTypeList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+
+
+namespace JokerBooksManager.Managers
+{
+    public partial class FrmBookTypeList : UIForm
+    {
+        public FrmBookTypeList()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+        #region 关闭窗体
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        private void BtnAddNew_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
