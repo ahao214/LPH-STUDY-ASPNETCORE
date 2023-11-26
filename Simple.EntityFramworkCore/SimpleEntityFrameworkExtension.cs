1a5520aef7269a2592c448c1584f9a8ad9c7510a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-26 18:22:45 +0800

    efcore核心扩展

---
+++ Simple.EntityFramworkCore/SimpleEntityFrameworkExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.EntityFramworkCore
+{
+    public static class SimpleEntityFrameworkExtension
+    {
+
+        /// <summary>
+        /// efcore核心扩展
+        /// </summary>
+        /// <typeparam name="IDbContext"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="options"></param>
+        /// <param name="lifetime"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddEFCore<IDbContext>(this IServiceCollection services, Action<DbContextOptionsBuilder>? options = null,
+            ServiceLifetime lifetime = ServiceLifetime.Singleton) where IDbContext : MasterDbContext<DbContext>
+        {
+            services.AddDbContext<IDbContext>(options, lifetime);
+            return services;
+        }
+
+
+    }
+}
