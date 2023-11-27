7a70d641bbc3a1d384f446f712e3419a377e4d58
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 21:28:18 +0800

    文件位置调整

---
+++ Blog.Application.Contract/User/Dto/CreateUserDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Contract.User.Dto
+{
+    public class CreateUserDto
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [MinLength(6, ErrorMessage = "用户名长度不能小于6位")]
+        public string UserName { get; set; } = null!;
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [MinLength(6, ErrorMessage = "密码长度不能小于6位")]
+        public string Password { get; set; } = null!;
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string? Avatar { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; set; }
+
+    }
+}
--- Blog.Application.Contract/User/IUserServer.cs
+++ Blog.Application.Contract/User/IUserServer.cs
@@ -1,4 +1,4 @@
-﻿using Blog.Application.Contract.Dto;
+﻿using Blog.Application.Contract.User.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
