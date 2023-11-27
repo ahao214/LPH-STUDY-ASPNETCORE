4a8a4673145da0254eae680a95975f84ea8e87fe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 16:15:54 +0800

    Jwt配置

---
+++ Ahao-Blog/Options/JwtOptions.cs
@@ -0,0 +1,25 @@
+﻿namespace Ahao_Blog.Options
+{
+    /// <summary>
+    /// Jwt配置
+    /// </summary>
+    public class JwtOptions
+    {
+        /// <summary>
+        /// 秘钥
+        /// </summary>
+        public string SecretKey { get; set; } = null!;
+        /// <summary>
+        /// 发行人
+        /// </summary>
+        public string Issuer { get; set; } = null!;
+        /// <summary>
+        /// 拥护者
+        /// </summary>
+        public string Audience { get; set; } = null!;
+        /// <summary>
+        /// 过期时间(分钟)
+        /// </summary>
+        public int ExpireMinute { get; set; }
+    }
+}
