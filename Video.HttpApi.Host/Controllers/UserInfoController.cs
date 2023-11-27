bd7c588e2b63405867f8babe81e9d26895b2cd3d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 11:23:14 +0800

    用户控制器

---
+++ Video.HttpApi.Host/Controllers/UserInfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Video.HttpApi.Host.Controllers
+{
+    /// <summary>
+    /// 用户控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfoController : ControllerBase
+    {
+    }
+}
