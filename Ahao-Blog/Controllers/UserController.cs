cbede1fd2ffeb3e780ab653d64373a11d870133d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 16:04:10 +0800

    user控制器

---
+++ Ahao-Blog/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Blog.Application.Contract.Dto;
+using Blog.Application.Contract.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ahao_Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServer _userServer;
+
+        public UserController(IUserServer userServer)
+        {
+            _userServer = userServer;
+        }
+
+        /// <summary>
+        /// 创建用户
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task CreateUserAsync(CreateUserDto input)
+        {
+            await _userServer.CreateUserAsync(input);
+        }
+
+
+
+
+    }
+}
