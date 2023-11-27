460faf5b916ea6e5441e015c1772df91f1c32e44
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 15:50:01 +0800

    异常过滤器

---
+++ Ahao-Blog/Filters/ExceptionFilter.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Ahao_Blog.Filters
+{
+    /// <summary>
+    /// 异常过滤器
+    /// </summary>
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger<ExceptionFilter> logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> log)
+        {
+            logger = log;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            var ex = context.Exception;
+
+            if (ex is BusinessException exceptions)
+            {
+                context.Result = new OkObjectResult(new HttpResult(exceptions.Code, exceptions.Message));
+            }
+            else
+            {
+                context.Result = new OkObjectResult(new HttpResult(500, ex.Message));
+            }
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ Infrastructure/HttpResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class HttpResult
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int Code { get; set; }
+        /// <summary>
+        /// 信息
+        /// </summary>
+        public string? Message { get; set; }
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public object? Data { get; set; }
+
+        public HttpResult(int code, string? message)
+        {
+            this.Code = code;
+            this.Message = message;       
+        }
+
+        public HttpResult(int code, string? message, object? data)
+        {
+            this.Code = code;
+            this.Message = message;
+            this.Data = data;
+        }
+    }
+}
