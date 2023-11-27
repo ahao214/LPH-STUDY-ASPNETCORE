ad791f5d896b87db10b4a0fb9feb54136a9fec3f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 15:37:57 +0800

    业务异常类

---
+++ Infrastructure/BusinessException.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure
+{
+    /// <summary>
+    /// 业务异常类
+    /// </summary>
+    public class BusinessException : Exception
+    {
+        public int Code { get; }
+        public BusinessException(string? message, int code = 400) : base(message)
+        {
+            Code = code;
+        }
+
+        
+
+
+
+    }
+}
