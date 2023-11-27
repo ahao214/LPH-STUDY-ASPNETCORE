f6888f9e6a7be31f3505f48ccc15a1906894991f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 16:52:33 +0800

    EntityDto

---
+++ Blog.Application.Contract/Base/EntityDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Contract.Base
+{
+    public class EntityDto
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+    }
+}
