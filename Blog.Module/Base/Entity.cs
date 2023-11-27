11259dd4abd9a501d56372359d0fd695eabba2df
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 14:18:09 +0800

    基础实体类

---
+++ Blog.Module/Base/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Module.Base
+{
+    /// <summary>
+    /// 基础实体类
+    /// </summary>
+    public class Entity
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+
+    }
+}
