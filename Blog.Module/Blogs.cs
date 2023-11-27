fc0908d715490d5975c4fbf96f71b654b4b1383f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 14:21:10 +0800

    博客文章

---
+++ Blog.Module/Blogs.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Module.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Module
+{
+    /// <summary>
+    /// 博客文章
+    /// </summary>
+    public class Blogs : Entity
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 文章内容
+        /// </summary>
+        public string? Content { get; set; }
+        /// <summary>
+        /// 浏览量
+        /// </summary>
+        public long PageView { get; set; }
+        /// <summary>
+        /// 文章类型ID
+        /// </summary>
+        public Guid TypeId { get; set; }
+
+    }
+}
