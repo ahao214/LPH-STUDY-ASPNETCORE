ecd38046343b23c5e9f89ee9af772624a9ee0014
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 14:28:36 +0800

    博客评论

---
+++ Blog.Module/BlogComment.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Module.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Module
+{
+    /// <summary>
+    /// 博客评论
+    /// </summary>
+    public class BlogComment:Entity
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public Guid UserId { get; set; }
+        /// <summary>
+        /// 博客ID
+        /// </summary>
+        public Guid BlogId { get; set; }
+        /// <summary>
+        /// 评论内容
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
