40fef8e670363644570d31fffb0e39ebc62ce2b0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 12:08:45 +0800

    视频

---
+++ Video.Application.Contract/Base/VideoInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Video.Application.Contract.Base
+{
+    /// <summary>
+    /// 视频
+    /// </summary>
+    public class VideoInput
+    {
+        public string? KeyWord { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+
+    }
+}
