497b67d6a30546a65166324e82ab636f87544eee
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 15:12:13 +0800

    UserAutoMapperProfile

---
+++ Blog.Application/AutoMapper/UserAutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.AutoMapper
+{
+    public class UserAutoMapperProfile:Profile
+    {
+        public UserAutoMapperProfile()
+        {
+                
+        }
+    }
+}
