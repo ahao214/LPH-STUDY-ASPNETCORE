ce96a9985ff124c7a4f1085a7a2ee4d0ee150491
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 14:25:57 +0800

    博客类型表

---
+++ Blog.Module/BlogTypes.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Module.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Module
+{
+    /// <summary>
+    /// 博客类型表
+    /// </summary>
+    public class BlogTypes : Entity
+    {
+        /// <summary>
+        /// 类型名称
+        /// </summary>
+        public string Name { get; set; }
+
+
+    }
+}
