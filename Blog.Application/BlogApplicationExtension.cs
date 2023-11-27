343dff039c3ea48d9f8d748f76e63604f654e2b2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 15:08:19 +0800

    注入AddApplication

---
+++ Blog.Application/BlogApplicationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Blog.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application
+{
+    public static class BlogApplicationExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddEntityFrameworkCore();
+        }
+
+    }
+}
