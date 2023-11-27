03efd1b4bad989963199562dcab03d605fcdb5e7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 12:06:24 +0800

    PageResultDto

---
+++ Video.Application.Contract/Base/PageResultDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Video.Application.Contract.Base
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PageResultDto<T>
+    {
+        public IReadOnlyList<T> Items { get; set; }
+
+        public int Count { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="items"></param>
+        public PageResultDto(int count, IReadOnlyList<T> items)
+        {
+            Items = items;
+            Count = count;
+        }
+
+    }
+
+}
