24fabd1260bac7a70d23598f004d733ea61ffbe4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-27 15:03:23 +0800

    BlogEntityFrameworkCoreExtension

---
+++ Blog.EntityFrameworkCore/BlogEntityFrameworkCoreExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.EntityFrameworkCore
+{
+    public static class BlogEntityFrameworkCoreExtension
+    {
+        public static void AddEntityFrameworkCore(this IServiceCollection services)
+        {
+            var configure = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+
+            services.AddDbContext<BlogDbContext>(options => options.UseMySql(configure["ConnectionStrings:Default"],new MySqlServerVersion(new Version (8,0,10))));
+
+
+        }
+
+    }
+}
