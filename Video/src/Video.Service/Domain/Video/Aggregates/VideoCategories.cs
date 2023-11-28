b0bec529a200e5d24b6c6beb1add7a715ac5d5f7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 16:49:15 +0800

    视频分类

---
+++ Video/src/Video.Service/Domain/Video/Aggregates/VideoCategories.cs
@@ -0,0 +1,17 @@
+﻿namespace Video.Service.Domain.Video.Aggregates
+{
+    /// <summary>
+    /// 视频分类
+    /// </summary>
+    public class VideoCategories : Entity<int>
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
