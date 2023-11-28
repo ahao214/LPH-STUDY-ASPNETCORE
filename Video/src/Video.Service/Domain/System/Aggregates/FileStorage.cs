375012364f63f4c2a4bbc584a63e68979696aa7a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 17:28:03 +0800

    文件名

--- Video/src/Video.Service/Domain/System/Aggregates/FileStorage.cs
+++ Video/src/Video.Service/Domain/System/Aggregates/FileStorage.cs
@@ -1,27 +1,64 @@
-﻿using Video.Contracts.Shared;
+﻿
 
 namespace Video.Service.Domain.System.Aggregates
 {
     /// <summary>
     /// 文件名
     /// </summary>
-    public class FileStorage:Entity<int>
+    public class FileStorage : Entity<int>
     {
+        public string name;
+        public string path;
+        public long size;
+        public FileType type;
         /// <summary>
         /// 文件名称
         /// </summary>
-        public string FileName { get; set; }
+        public string Name { get => name; set => name = value; }
         /// <summary>
         /// 文件路径
         /// </summary>
-        public string FilePath { get; set; }
+        public string Path { get => path; set => path = value; }
         /// <summary>
         /// 文件大小
         /// </summary>
-        public long FileSize { get; set; }
+        public long Size { get => size; set => size = value; }
         /// <summary>
         /// 文件类型
         /// </summary>
-        public FileType FileType { get; set; }
+        public FileType Type { get => type; private set => type = value; }
+
+
+        protected FileStorage()
+        {
+
+        }
+
+        public FileStorage(string name, string path, long size, FileType type)
+        {
+            UpdateName(name);
+            UpdatePath(path);
+            Size = size;
+            Type = type;
+        }
+
+        public void UpdateName(string name)
+        {
+            if(name.IsNullOrWhiteSpace())
+            {
+                throw new UserFriendlyException("文件名称不能为空");
+            }
+            Name = name;
+        }
+
+        public void UpdatePath(string path)
+        {
+            if(path.IsNullOrWhiteSpace())
+            {
+                throw new UserFriendlyException("文件路径不能为空");
+            }
+            Path = path;
+        }
+
     }
 }
