a02a2cf254da9c9976d1d5db19d6b056e04ca7c8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 23:17:27 +0800

    数据访问层：作者

---
+++ JokerBooksManagerDAL/BookDAL/AuthorDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerDAL.BookDAL
+{
+    /// <summary>
+    /// 数据访问层：作者
+    /// </summary>
+    public class AuthorDAL
+    {
+
+
+
+    }
+}
