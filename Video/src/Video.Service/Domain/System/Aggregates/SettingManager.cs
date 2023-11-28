de0e110c61a3be66771ee7c2a6774fd98463ed4a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 19:10:17 +0800

    SettingManager

---
+++ Video/src/Video.Service/Domain/System/Aggregates/SettingManager.cs
@@ -0,0 +1,31 @@
+﻿namespace Video.Service.Domain.System.Aggregates
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SettingManager : Entity<long>
+    {
+        private string name;
+        private string value;
+        /// <summary>
+        /// 扩展参数
+        /// </summary>
+        public string Name { get => name; private set => name = value; }
+        /// <summary>
+        /// 请求耗时(ms)
+        /// </summary>
+        public string Value { get => value; private set => this.value = value; }
+
+        protected SettingManager()
+        {
+
+        }
+
+        public SettingManager(string name, string value)
+        {
+            Name = name;
+            Value = value;
+
+        }
+    }
+}
