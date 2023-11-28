d64bc94094cb3533547c9d5da33d770d74e8fa64
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 16:47:25 +0800

    点赞表

---
+++ Video/src/Video.Service/Domain/Video/Aggregates/VideoLikes.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace Video.Service.Domain.Video.Aggregates
+{
+    /// <summary>
+    /// 点赞表
+    /// </summary>
+    public class VideoLikes
+    {
+        public long Id { get; set; }
+        /// <summary>
+        /// 视频ID
+        /// </summary>
+        public long VideoId { get; set; }
+        /// <summary>
+        /// 点赞用户ID
+        /// </summary>
+        public long UserId { get; set; }
+    }
+}
