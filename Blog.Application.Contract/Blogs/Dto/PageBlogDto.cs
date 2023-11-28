e799854439ff398fea1bf1252e97c2bb24b0874b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 12:46:47 +0800

    PageBlogDto

---
+++ Blog.Application.Contract/Blogs/Dto/PageBlogDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Contract.Blogs.Dto
+{
+    public class PageBlogDto
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string? Title { get; set; }
+        /// <summary>
+        /// 浏览量
+        /// </summary>
+        public long PageView { get; set; }
+        /// <summary>
+        /// 点赞量
+        /// </summary>
+        public long Like { get; set; }
+        /// <summary>
+        /// 文章类型ID
+        /// </summary>
+        public Guid TypeId { get; set; }
+        /// <summary>
+        /// 作者ID
+        /// </summary>
+        public Guid AuthorId { get; set; }
+        /// <summary>
+        /// 导航属性用户
+        /// </summary>
+        public BlogUserDto? Author { get; set; }
+        /// <summary>
+        /// 导航属性文章类型
+        /// </summary>
+        public BlogTypeDto? Type { get; set; }
+
+
+
+
+
+    }
+}
