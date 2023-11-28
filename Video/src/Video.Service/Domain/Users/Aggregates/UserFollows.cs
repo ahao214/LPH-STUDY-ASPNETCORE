312ef44d7110f597c594130f6d633e32a94d6c09
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 17:07:23 +0800

    用户粉丝

---
+++ Video/src/Video.Service/Domain/Users/Aggregates/UserFollows.cs
@@ -0,0 +1,20 @@
+﻿namespace Video.Service.Domain.Users.Aggregates
+{
+    /// <summary>
+    /// 用户粉丝
+    /// </summary>
+    public class UserFollows : Entity<long>
+    {
+        /// <summary>
+        /// 关注者ID
+        /// </summary>
+        public long FollowerId { get; set; }
+        /// <summary>
+        /// 被关注者ID
+        /// </summary>
+        public long FollowedId { get; set; }
+    }
+
+
+
+}
