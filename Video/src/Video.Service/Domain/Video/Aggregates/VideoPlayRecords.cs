3ab756c2cc9a6b9840e6c7c69fd2df61489fea01
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 17:13:04 +0800

    视频播放记录

---
+++ Video/src/Video.Service/Domain/Video/Aggregates/VideoPlayRecords.cs
@@ -0,0 +1,44 @@
+﻿using Masa.BuildingBlocks.Ddd.Domain.Entities.Auditing;
+
+namespace Video.Service.Domain.Video.Aggregates
+{
+    /// <summary>
+    /// 视频播放记录
+    /// </summary>
+    public class VideoPlayRecords : Entity<long>, IAuditEntity<long>
+    {
+        public long videoId;
+        public DateTime playTime;
+
+        /// <summary>
+        /// 视频ID
+        /// </summary>
+        public long VideoId { get => videoId; private set => videoId = value; }
+        /// <summary>
+        /// 播放时间
+        /// </summary>
+        public DateTime PlayTime { get => playTime; private set => playTime = value; }
+
+        public long Creator { get; private set; }
+
+        public DateTime CreationTime { get; private set; }
+
+        public long Modifier { get; private set; }
+
+        public DateTime ModificationTime { get; private set; }
+
+
+        public VideoPlayRecords()
+        {
+
+        }
+
+        public VideoPlayRecords(long videoId, DateTime playTime)
+        {
+            VideoId = videoId;
+            PlayTime = playTime;
+
+        }
+    }
+
+}
