6f30ce99a54f5ef0add276d528d5579b18dd9a24
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 12:32:19 +0800

    分页模型

---
+++ Blog.Application.Contract/Base/PageResponseDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Contract.Base
+{
+    /// <summary>
+    /// 分页模型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PageResponseDto<T>
+    {
+        public PageResponseDto()
+        {
+        }
+
+        public int Total { get; set; }
+
+        public IReadOnlyList<T> Items { get; set; }
+
+        public PageResponseDto(int total, IReadOnlyList<T> items)
+        {
+            Total = total;
+            Items = items;
+        }
+    }
+}
