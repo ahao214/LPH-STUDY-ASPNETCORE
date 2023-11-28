7899f5146f3890f22beb36d2edffe638d03a9fda
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 16:46:12 +0800

    RoleUsers

---
+++ Video/src/Video.Service/Domain/System/Aggregates/RoleUsers.cs
@@ -0,0 +1,14 @@
+﻿namespace Video.Service.Domain.System.Aggregates
+{
+    public class RoleUsers : Entity<long>
+    {
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public long UserId { get; set; }
+        /// <summary>
+        /// 角色ID
+        /// </summary>
+        public long RoleId { get; set; }
+    }
+}
