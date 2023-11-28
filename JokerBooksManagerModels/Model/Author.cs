6273bfc063cb9e36a40b3a80c5a9b960e883e697
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 23:15:58 +0800

    作者

---
+++ JokerBooksManagerModels/Model/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerModels.Model
+{
+    /// <summary>
+    /// 作者
+    /// </summary>
+    public class Author
+    {
+        /// <summary>
+        /// 作者ID
+        /// </summary>
+        public int AuthorId { get; set; }
+        /// <summary>
+        /// 作者名称
+        /// </summary>
+        public string AuthorName { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
