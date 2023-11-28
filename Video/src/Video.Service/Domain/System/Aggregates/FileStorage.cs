e0bf107dd7e994e2980fbfa31cdf56ede74143a2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 17:17:55 +0800

    文件名

---
+++ Video/src/Video.Service/Domain/System/Aggregates/FileStorage.cs
@@ -0,0 +1,27 @@
+﻿using Video.Contracts.Shared;
+
+namespace Video.Service.Domain.System.Aggregates
+{
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public class FileStorage:Entity<int>
+    {
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string FilePath { get; set; }
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public long FileSize { get; set; }
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public FileType FileType { get; set; }
+    }
+}
