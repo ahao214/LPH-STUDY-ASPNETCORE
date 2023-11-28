8aa42baf427a4929b8919da7e8467d69feffc5cd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 23:36:38 +0800

    作者信息列表窗体

---
+++ JokerBooksManager/Managers/FrmAuthorList.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace JokerBooksManager.Managers
+{
+    partial class FrmAuthorList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmAuthorList
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBoxFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.Name = "FrmAuthorList";
+            this.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Style = Sunny.UI.UIStyle.Green;
+            this.Text = "作者信息列表";
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.ZoomScaleRect = new System.Drawing.Rectangle(22, 22, 800, 450);
+            this.Load += new System.EventHandler(this.FrmAuthorList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ JokerBooksManager/Managers/FrmAuthorList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+
+
+namespace JokerBooksManager.Managers
+{
+    /// <summary>
+    /// 作者信息列表窗体
+    /// </summary>
+    public partial class FrmAuthorList : UIForm
+    {
+        public FrmAuthorList()
+        {
+            InitializeComponent();
+        }
+
+        #region 窗体加载
+        private void FrmAuthorList_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
