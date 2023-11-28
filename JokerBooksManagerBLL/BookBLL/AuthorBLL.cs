fbaac92e908e595a0d39cc289d6853969773bf20
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-28 23:16:43 +0800

    业务逻辑层：作者

---
+++ JokerBooksManagerBLL/BookBLL/AuthorBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerBLL.BookBLL
+{
+    /// <summary>
+    /// 业务逻辑层：作者
+    /// </summary>
+    public class AuthorBLL
+    {
+    }
+}
