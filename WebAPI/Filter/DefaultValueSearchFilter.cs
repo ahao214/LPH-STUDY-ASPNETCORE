3766a1b349bf135258bbb6a3c965f2ecdc63caf9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-29 14:40:25 +0800

    过滤器

---
+++ WebAPI/Filter/DefaultValueSearchFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WebAPI.Filter
+{
+    /// <summary>
+    /// 过滤器
+    /// </summary>
+    public class DefaultValueSearchFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (schema == null)
+            {
+                return;
+            }
+            var objectSchema = schema;
+            foreach (var property in objectSchema.Properties)
+            {
+                if (property.Value.Type == "string" && property.Value.Default == null)
+                {
+                    property.Value.Default = new OpenApiString("");
+                }
+            }
+
+
+        }
+    }
+}
