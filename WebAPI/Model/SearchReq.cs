5900d5ec0ba4e5a09807f763045e19e1be59e722
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-29 14:48:29 +0800

    通过特性设置参数默认值

--- WebAPI/Model/SearchReq.cs
+++ WebAPI/Model/SearchReq.cs
@@ -1,11 +1,14 @@
-﻿namespace WebAPI.Model
+﻿using System.ComponentModel;
+
+namespace WebAPI.Model
 {
     public class SearchReq
     {
+        [DefaultValue("zhangsan")]
         public string Name { get; set; }
-
+        [DefaultValue(1)]
         public int PageIndex { get; set; }
-
+        [DefaultValue(10)]
         public int PageSize { get; set; }
 
     }
