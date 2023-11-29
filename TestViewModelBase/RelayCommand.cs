6ac4f4cb23e3dc5c24c4896dca319b4a9a762e6d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-29 22:59:57 +0800

    command

--- TestViewModelBase/RelayCommand.cs
+++ TestViewModelBase/RelayCommand.cs
@@ -34,4 +34,27 @@ namespace TestViewModelBase
             _execute();
         }
     }
+
+
+
+    public class RelayCommand<T> : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+
 }
