f433581e57c89f258e3651df076c0b837ab13f80
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-29 21:28:17 +0800

    图书信息类

---
+++ JokerBooksManagerModels/Model/BookInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerModels.Model
+{
+    /// <summary>
+    /// 图书信息类
+    /// </summary>
+    public class BookInfo
+    {
+
+
+    }
+}
