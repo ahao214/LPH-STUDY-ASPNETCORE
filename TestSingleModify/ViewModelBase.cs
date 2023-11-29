5f1025817000e5ff19b7ae50882e302f5082e0f8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-29 23:12:19 +0800

    singlemove

---
+++ TestSingleModify/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSingleModify
+{
+    public class ViewModelBase:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+
+        public void RaisePropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            MemberExpression member = (MemberExpression)expression.Body;
+            string propName = member.Member.Name;
+            RaisePropertyChanged(propName);
+
+        }
+
+
+    }
+}
