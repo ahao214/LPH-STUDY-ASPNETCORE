be40fdcd567f9cbb7a0126b198f848c1974df7a3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-29 14:51:55 +0800

    反序列化和序列化

--- WebAPI/Controllers/ValuesController.cs
+++ WebAPI/Controllers/ValuesController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Model;
-
+using System.Text.Json;
 
 
 
@@ -14,7 +14,15 @@ namespace WebAPI.Controllers
         [HttpGet]
         public string Test()
         {
-            return "ok";
+            List<TestModel> list = new List<TestModel>();
+            list.Add(new TestModel() { Id = 1, Name = "jack", Description = "aaa", CreateTime = DateTime.Now });
+            list.Add(new TestModel() { Id = 2, Name = "kutng", Description = "dd", CreateTime = DateTime.Now });
+
+            string str = JsonSerializer.Serialize(list);
+            // 反序列化
+            var list2 = JsonSerializer.Deserialize<TestModel>(str);
+            // 序列化
+            return JsonSerializer.Serialize(list);
         }
 
         [HttpGet]
