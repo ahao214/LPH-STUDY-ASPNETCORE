db5354cc556bd7e876f59c78c28cbdf9d0ccaa24
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 20:45:36 +0800

    增加

--- E3/Program.cs
+++ E3/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using E3.Model;
+using static System.Console;
+
 
 namespace E3
 {
@@ -10,6 +13,24 @@ namespace E3
     {
         static void Main(string[] args)
         {
+            using (FreshLiveDB db = new FreshLiveDB())
+            {
+                // 增加
+                ProductClass pc = new ProductClass()
+                {
+                    Name = "C#班"
+                };
+
+                db.ProductClass.Add(pc);
+                db.SaveChanges();
+
+
+
+            }
+
+
+            ReadLine();
+
         }
     }
 }
