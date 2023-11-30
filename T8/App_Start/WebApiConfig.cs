80af9677bb700f0db54b1189c4bc004d0a71a510
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 14:04:53 +0800

    WebApiConfig

--- T8/App_Start/WebApiConfig.cs
+++ T8/App_Start/WebApiConfig.cs
@@ -9,6 +9,15 @@ namespace T8
     {
         public static void Register(HttpConfiguration config)
         {
+            // 移除原本的XML解析
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // 设置JSON序列化遇到循环引用的处理方式
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+
+
+
             // Web API 配置和服务
 
             // Web API 路由
--- T8/Controllers/ProductController.cs
+++ T8/Controllers/ProductController.cs
@@ -4,31 +4,32 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using T8.Models;
 
 namespace T8.Controllers
 {
     public class ProductController : ApiController
     {
+        FreshLiveDB db = new FreshLiveDB();
         // GET: api/Product
-        public IEnumerable<string> Get()
+        public IEnumerable<ProductClass> Get()
         {
-            return new string[] { "value1", "value2" };
+            return db.ProductClass.ToList();
         }
 
         // GET: api/Product/5
-        public string Get(int id)
+        public ProductClass Get(int id)
         {
-            return "value";
+            return db.ProductClass.Find(id);
         }
 
         // POST: api/Product
-        public void Post([FromBody]string value)
+        public void Post([FromBody] string value)
         {
         }
 
         // PUT: api/Product/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
