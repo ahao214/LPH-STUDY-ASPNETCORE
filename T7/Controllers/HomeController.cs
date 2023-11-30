c9bd9f8b9f669834f5cb81c1f9b4c267163953c6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 16:26:52 +0800

    color

--- T7/Controllers/HomeController.cs
+++ T7/Controllers/HomeController.cs
@@ -8,10 +8,36 @@ namespace T7.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult Index(string theme)
+        {
+            switch(theme)
+            {    
+                case "2":
+                    return Content("body{background-color:cyan;}", "text/css");
+                case "3":
+                    return Content("body{background-color:blue;}", "text/css");
+                default:
+                    return Content("body{background-color:purple;}", "text/css");
+            }
+
+            return View();
+
+        }
+
+
+
+
+
+
+
 
         public ActionResult About()
         {
