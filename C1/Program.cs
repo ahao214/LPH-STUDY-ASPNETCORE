2dfd91090f6bdd107983a4a6ca889890d6e0c41f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 20:16:15 +0800

    修改命名空间

--- C1/Program.cs
+++ C1/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
 
 
-namespace C1
+namespace E1
 {
     internal class Program
     {
--- C1/Water.cs
+++ C1/Water.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace C1
+﻿namespace E1
 {
     public delegate void WaterDelegate();
 
