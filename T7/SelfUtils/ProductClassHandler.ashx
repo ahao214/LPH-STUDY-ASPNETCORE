cb6f5c40514ef1ea3aa828e60701023c7999b051
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 12:04:54 +0800

    ProductClassHandler

---
+++ T7/SelfUtils/ProductClassHandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="ProductClassHandler.ashx.cs" Class="T7.SelfUtils.ProductClassHandler" %>
---
+++ T7/SelfUtils/ProductClassHandler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T7.SelfUtils
+{
+    /// <summary>
+    /// ProductClassHandler 的摘要说明
+    /// </summary>
+    public class ProductClassHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
