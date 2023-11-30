1c0cb64bba2883aeab9cf1d0e47c8643b7c65b41
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 17:17:41 +0800

    委托

---
+++ C1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace C1
+{
+    internal class Program
+    {
+        // 1 委托声明
+        public delegate string SayDelegate(string s);
+
+        public delegate void EatAndWatch(string name);
+        static void Main(string[] args)
+        {
+            //2 调用委托
+            SayDelegate sd = new SayDelegate(Say);
+            Console.WriteLine(sd.Invoke("jack"));
+
+            // 多播委托
+            EatAndWatch ewd = Eating;
+            ewd += Watching;
+            ewd("jack");
+
+
+            Console.ReadLine();
+        }
+
+        public static string Say(string s)
+        {
+            return "hello " + s;
+        }
+
+
+        public static void Eating(string name)
+        {
+            WriteLine(name + "正在吃饭");
+        }
+
+        public static void Watching(string name)
+        {
+            WriteLine(name + "正在看电视");
+        }
+
+    }
+}
