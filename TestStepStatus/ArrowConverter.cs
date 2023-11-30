0e7dbbe66527739b1ecfc3a5d220bc6340c716b4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 21:42:51 +0800

    StepConverter

--- TestStepStatus/ArrowConverter.cs
+++ TestStepStatus/ArrowConverter.cs
@@ -17,7 +17,28 @@ namespace TestStepStatus
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value == null || parameter == null)
+            {
+                return null;
+            }
+            int step = int.Parse(value.ToString());
+            int imgStep = int.Parse(parameter.ToString());
+            BitmapImage bitmap;
+            if (step < imgStep)
+            {                
+                string path = @"pack://application:,,,/TestStepStatus:component/Images/1.png";
+                bitmap = new BitmapImage(new Uri(path, UriKind.Absolute));
+                return bitmap;
+            }
+            else
+            {                
+                string path = @"pack://application:,,,/TestStepStatus:component/Images/2.png";
+                bitmap = new BitmapImage(new Uri(path, UriKind.Absolute));
+                return bitmap;
+            }
+
+
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
