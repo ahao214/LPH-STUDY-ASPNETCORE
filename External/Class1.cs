21ddf1c357c732a46fd48c0b8ab8cd72e145d266
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 17:30:45 +0800

    WPF使用Uri获取外部程序集的资源图片

---
+++ External/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace External
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TestUriExternal/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TestUriExternal
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
