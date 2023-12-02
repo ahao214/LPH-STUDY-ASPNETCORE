1b1d723d6548ed5114cf24a49f11f5882821a21c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 13:01:52 +0800

    create files

---
+++ YDT_IOCS/IOCS/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YDT_IOCS.IOCS
+{
+    public class Teacher
+    {
+    }
+}
