489b620e3e74d08e5a7986f5d4bb0a0b20216255
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 09:41:46 +0800

    页面设计

---
+++ TestMenu/ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+namespace TestMenu
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void RaisePropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            MemberExpression member = (MemberExpression)expression.Body;
+            string propName = member.Member.Name;
+            RaisePropertyChanged(propName);
+
+        }
+
+
+    }
+}
