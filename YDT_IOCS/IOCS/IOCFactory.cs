a1136bb57c7a4c5de4c2a5aa81bf488b77fc7551
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 13:56:20 +0800

    3 获取对象

--- YDT_IOCS/IOCS/IOCFactory.cs
+++ YDT_IOCS/IOCS/IOCFactory.cs
@@ -10,9 +10,16 @@ namespace YDT_IOCS.IOCS
 {
     /// <summary>
     /// IOC工厂
+    /// 1 创建对象
+    /// 2 存储对象：三种容器(List Set Dictionary) 使用 Dictionary
+    /// 3 获取对象
     /// </summary>
     public class IOCFactory
     {
+        // 创建字典
+        private Dictionary<string, object> IOCS = new Dictionary<string, object>();
+
+
         public IOCFactory()
         {
             Assembly assembly = Assembly.Load("YDT_IOCS");
@@ -22,8 +29,25 @@ namespace YDT_IOCS.IOCS
                 // 1 创建对象
                 object _object = Activator.CreateInstance(type);
 
+                // 2 存储对象
+                IOCS.Add(type.FullName, _object);
+
+
+
+
+
             }
 
+
+        }
+
+
+        /// <summary>
+        /// 3 获取对象
+        /// </summary>
+        public object GetObject(string fullName)
+        {
+            return IOCS[fullName];
 
         }
 
