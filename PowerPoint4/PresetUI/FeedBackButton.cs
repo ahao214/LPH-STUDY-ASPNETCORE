91186efc3cb1176917df65d2aaa15bdd437754a2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 09:06:55 +0800

    XAML命名空间

---
+++ PowerPoint4/PresetUI/FeedBackButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace PowerPoint4.PresetUI
+{
+    public class FeedBackButton
+    {
+        public Type PagesWindow { get; set; }
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+            Window win = Activator.CreateInstance(this.PagesWindow) as Window;
+            if (win != null)
+            {
+                win.Show();
+            }
+
+        }
+
+
+    }
+}
