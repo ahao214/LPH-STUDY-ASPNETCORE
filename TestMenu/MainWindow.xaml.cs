26ab0f51dc479ab75e07d56eb5eec99f07dcb8bd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 10:17:50 +0800

    菜单

--- TestMenu/MainWindow.xaml.cs
+++ TestMenu/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace TestMenu
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+
         }
     }
 }
---
+++ TestMenu/MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMenu
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<Department> departments = new ObservableCollection<Department>();
+
+        public ObservableCollection<Department> Departments
+        {
+            get
+            {
+                return departments;
+            }
+            set
+            {
+                departments = value;
+                RaisePropertyChanged(() => Departments);
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Departments = new ObservableCollection<Department>()
+            {
+                new Department()
+                {
+                    Name ="部门1",
+                    ImgPath="Images/OS3.pgn"
+                },
+                new Department()
+                {
+                    Name ="部门2",
+                    ImgPath="Images/OS3.pgn"
+                },
+                new Department()
+                {
+                    Name ="部门3",
+                    ImgPath="Images/OS3.pgn"
+                },
+                new Department()
+                {
+                    Name ="部门4",
+                    ImgPath="Images/OS3.pgn"
+                },
+                new Department()
+                {
+                    Name ="部门5",
+                    ImgPath="Images/OS3.pgn"
+                },
+                new Department()
+                {
+                    Name ="部门6",
+                    ImgPath="Images/OS3.pgn"
+                },
+                new Department()
+                {
+                    Name ="部门7",
+                    ImgPath="Images/OS3.pgn"
+                },
+            };
+        }
+
+
+    }
+}
