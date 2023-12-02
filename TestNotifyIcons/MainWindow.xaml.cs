43d618280c29d6a084379a930ea01aca29ee5552
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 11:47:15 +0800

    WPF程序如何设置任务栏和系统托盘图标

--- TestNotifyIcons/MainWindow.xaml.cs
+++ TestNotifyIcons/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+using System;
 
 namespace TestNotifyIcons
 {
@@ -20,9 +10,44 @@ namespace TestNotifyIcons
     /// </summary>
     public partial class MainWindow : Window
     {
+        NotifyIcon icon;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitIcon();
+        }
+
+
+        private void InitIcon()
+        {
+            icon = new NotifyIcon();
+            icon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+
+            icon.Text = "我的应用程序";
+            icon.Visible = true;
+
+            // 右键菜单命令
+            MenuItem menuItem = new MenuItem("退出");
+            menuItem.Click += MenuItem_Click;
+
+            MenuItem mi1 = new MenuItem("关于");
+            mi1.Click += Mi1_Click;
+
+            System.Windows.Forms.MenuItem[] ms = new System.Windows.Forms.MenuItem[] { menuItem, mi1 };
+            icon.ContextMenu = new System.Windows.Forms.ContextMenu(ms);
+
+        }
+
+        private void Mi1_Click(object sender, EventArgs e)
+        {
+            System.Windows.MessageBox.Show("关于程序的信息");
+        }
+
+        private void MenuItem_Click(object sender, EventArgs e)
+        {
+            icon.Dispose();
+            this.Close();
         }
     }
 }
