70b9ae090bb4d950ad70a75f242bb82736502af9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 19:07:47 +0800

    登录页面样式

--- LoginPageType1_Demo/MainWindow.xaml.cs
+++ LoginPageType1_Demo/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace LoginPageType1_Demo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
