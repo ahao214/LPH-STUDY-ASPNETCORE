2b5cf2d1970d8ecdd957cf5be9605a1e010f2483
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 14:31:39 +0800

    使用IOC

--- YDT_IOCS/Program.cs
+++ YDT_IOCS/Program.cs
@@ -1,9 +1,22 @@
-﻿using static System.Console;
+﻿using YDT_IOCS.IOCS;
+using static System.Console;
 
 Title = "IOC源码";
 
 
+// 创建IOCFactory
+IOCFactory iOCFactory = new IOCFactory();
 
+// 创建对象
+Student stu = (Student)iOCFactory.GetObject("YDT_IOCS.IOCS.Student");
+
+// 使用student
+stu.Study();
+
+Teacher tea = (Teacher)iOCFactory.GetObject("YDT_IOCS.IOCS.Teacher");
+tea.Classes();
+
+ReadKey();
 
 
 
