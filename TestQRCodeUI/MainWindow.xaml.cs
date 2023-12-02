057ef61e0039f12752ffefbe8aa12f2ca0c60936
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 18:06:27 +0800

    生成二维码

--- TestQRCodeUI/MainWindow.xaml.cs
+++ TestQRCodeUI/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using QRCoder;
 
 namespace TestQRCodeUI
 {
@@ -23,11 +27,41 @@ namespace TestQRCodeUI
         public MainWindow()
         {
             InitializeComponent();
+            txt.Text = "www.baidu.com";
+
         }
 
         private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            CreateQRCode();
+        }
+
+
+        private void CreateQRCode()
+        {
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(txt.Text, QRCodeGenerator.ECCLevel.H);
+
+            QRCode code = new QRCode(qRCodeData);
+
+            Bitmap bitmap = code.GetGraphic(15, System.Drawing.Color.Black, System.Drawing.Color.White, true);
+
+            BitmapImage image = new BitmapImage();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Bmp);
+                ms.Position = 0;
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = ms;
+                image.EndInit();
+
+            };
+            img.Source = image;
 
         }
+
+
+
     }
 }
