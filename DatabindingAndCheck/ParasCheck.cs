3596d987c70fe3d4f2df9b76b85771233d2b49ce
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-03 17:30:58 +0800

    数据校验

---
+++ DatabindingAndCheck/ParasCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DatabindingAndCheck
+{
+    /// <summary>
+    /// 数据校验
+    /// </summary>
+    public class ParasCheck : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            double num;
+            if (double.TryParse(value.ToString(), out num))
+            {
+                if (num == 0)
+                {
+                    return new ValidationResult(false, "除数不能为零");
+                }
+            }
+            return ValidationResult.ValidResult;
+
+        }
+
+    }
+}
