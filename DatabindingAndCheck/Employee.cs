4341e4e71680204dcfe0097aca181b7977c7e580
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-03 16:35:05 +0800

    add 字段

--- DatabindingAndCheck/Employee.cs
+++ DatabindingAndCheck/Employee.cs
@@ -7,23 +7,67 @@ using System.Threading.Tasks;
 
 namespace DatabindingAndCheck
 {
-    public class Employee:INotifyPropertyChanged
+    public class Employee : INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                // 在属性值变更后，激发 PropertyChanged 事件
+                // 参数1：事件来源
+                // 参数2：属性变更事件参数
+                if (this.PropertyChanged != null)    // 无订阅判断，避免激发空事件产生异常
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Id"));
+            }
+        }
+
         string name;
         public string Name
         {
             get { return name; }
-            set 
+            set
             {
                 name = value;
                 // 在属性值变更后，激发 PropertyChanged 事件
                 // 参数1：事件来源
                 // 参数2：属性变更事件参数
-                if(this.PropertyChanged != null)    // 无订阅判断，避免激发空事件产生异常
+                if (this.PropertyChanged != null)    // 无订阅判断，避免激发空事件产生异常
                     PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
             }
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+
+                if (this.PropertyChanged != null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Age"));
+            }
+        }
+
+        string phoneType;
+        public string PhoneType
+        {
+            get { return phoneType;}
+            set
+            {
+                phoneType = value;
+                if (this.PropertyChanged != null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PhoneType"));
+
+            }
+        }
+
     }
 }
