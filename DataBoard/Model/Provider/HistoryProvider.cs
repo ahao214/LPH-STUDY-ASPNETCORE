f31233956a9d52619a4b44cf33a243e695f51541
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 23:36:24 +0800

    HistoryProvider

--- DataBoard/Model/Provider/HistoryProvider.cs
+++ DataBoard/Model/Provider/HistoryProvider.cs
@@ -11,24 +11,28 @@ namespace DataBoard.Model.Provider
     /// </summary>
     public class HistoryProvider : IProvider<History>
     {
+        private readonly BoardDBEntities db = new BoardDBEntities();
         public int Delete(History t)
         {
-            throw new NotImplementedException();
+            db.Entry(t).State = System.Data.Entity.EntityState.Deleted;
+            return db.SaveChanges();
         }
 
         public int Insert(History t)
         {
-            throw new NotImplementedException();
+            db.Entry(t).State = System.Data.Entity.EntityState.Added;
+            return db.SaveChanges();
         }
 
         public List<History> Select()
         {
-            throw new NotImplementedException();
+            return db.History.Include("Line").Include("StopType").Include("SubLine").Include("UserInfo").ToList();
         }
 
         public int Update(History t)
         {
-            throw new NotImplementedException();
+            db.Entry(t).State = System.Data.Entity.EntityState.Modified;
+            return db.SaveChanges();
         }
     }
 }
