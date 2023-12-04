ad17ff02e863e1422317abc47192e4a95e163c63
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 14:21:22 +0800

    自定义控件：TextBox

---
+++ SingleControlLibrbay/Controls/RegTextBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+
+namespace SingleControlLibrbay.Controls
+{
+    /// <summary>
+    /// 自定义控件：TextBox
+    /// </summary>
+    public class RegTextBox : TextBox
+    {
+
+    }
+}
