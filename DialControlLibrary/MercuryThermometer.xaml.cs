1f895dca3557e395db030f52945d72373fc1edd3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 12:51:53 +0800

    依赖属性

--- DialControlLibrary/MercuryThermometer.xaml.cs
+++ DialControlLibrary/MercuryThermometer.xaml.cs
@@ -24,5 +24,47 @@ namespace DialControlLibrary
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 温度计最大值
+        /// </summary>
+        public int Maximun
+        {
+            get { return (int)GetValue(MaximunProperty); }
+            set { SetValue(MaximunProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximunProperty = DependencyProperty.Register("Maximun", typeof(int), typeof(MercuryThermometer), new PropertyMetadata(50));
+
+
+
+        /// <summary>
+        /// 温度计最小值
+        /// </summary>
+        public int Minimun
+        {
+            get { return (int)GetValue(MinimunProperty); }
+            set { SetValue(MinimunProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinimunProperty =
+            DependencyProperty.Register("Minimun", typeof(int), typeof(MercuryThermometer), new PropertyMetadata(-20));
+
+
+        /// <summary>
+        /// 温度计显示数值
+        /// </summary>
+        public int DisplayValue
+        {
+            get { return (int)GetValue(DisplayValueProperty); }
+            set { SetValue(DisplayValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisplayValueProperty =
+            DependencyProperty.Register("DisplayValue", typeof(int), typeof(MercuryThermometer), new PropertyMetadata(0));
+
+
+
+
     }
 }
