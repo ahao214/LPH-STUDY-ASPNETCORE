932a2234c61b050297afd9af3826f2403efc933c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 14:20:41 +0800

    自定义控件：Label

---
+++ SingleControlLibrbay/Controls/RegLabel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SingleControlLibrbay.Controls
+{
+    /// <summary>
+    /// 自定义控件：Label
+    /// </summary>
+    public class RegLabel:Label
+    {
+    }
+}
