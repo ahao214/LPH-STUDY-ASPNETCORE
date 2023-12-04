fc1da82f61e847cd1bfceb54d1b0a5228f229ba4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 10:46:13 +0800

    Delete 不必要的程序

---
+++ YDT_EBusiness/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YDT_EBusiness.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        
+        /// <summary>
+        /// 查询商品接口
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name ="GetProduct")]
+        public string GetProduct()
+        {
+            // 日志
+            _logger.LogInformation("查询商品成功");
+
+            return "查询商品成功";
+        }
+
+
+
+
+    }
+}
