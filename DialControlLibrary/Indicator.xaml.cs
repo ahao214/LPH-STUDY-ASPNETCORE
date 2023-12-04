0b7b240920b9aadd2460fe76ba7d0d90094f77ce
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 13:32:15 +0800

    依赖属性

--- DialControlLibrary/Indicator.xaml.cs
+++ DialControlLibrary/Indicator.xaml.cs
@@ -27,5 +27,60 @@ namespace DialControlLibrary
 
 
 
+        public int MaxMark
+        {
+            get { return (int)GetValue(MaxMarkProperty); }
+            set { SetValue(MaxMarkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MaxMark.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxMarkProperty =
+            DependencyProperty.Register("MaxMark", typeof(int), typeof(Indicator), new PropertyMetadata(50, new PropertyChangedCallback(ValueChanged)));
+
+
+
+        public int MinMark
+        {
+            get { return (int)GetValue(MinMarkProperty); }
+            set { SetValue(MinMarkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MinMark.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinMarkProperty =
+            DependencyProperty.Register("MinMark", typeof(int), typeof(Indicator), new PropertyMetadata(-20, new PropertyChangedCallback(ValueChanged)));
+
+
+
+
+
+        public int DisplayValue
+        {
+            get { return (int)GetValue(DisplayValueProperty); }
+            set { SetValue(DisplayValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DisplayValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DisplayValueProperty =
+            DependencyProperty.Register("DisplayValue", typeof(int), typeof(Indicator), new PropertyMetadata(0, new PropertyChangedCallback(ValueChanged)));
+
+
+
+
+
+
+        private static void ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Indicator)d).LightContorl();
+
+        }
+
+        /// <summary>
+        /// 控制灯
+        /// </summary>
+        private void LightContorl()
+        {
+
+        }
+
     }
 }
