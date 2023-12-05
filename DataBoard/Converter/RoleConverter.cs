049981d023a6fbc297dc228f0dd1e9f4df8c673a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-05 12:11:33 +0800

    角色转换器

---
+++ DataBoard/Converter/RoleConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataBoard.Converter
+{
+    /// <summary>
+    /// 角色转换器
+    /// </summary>
+    public class RoleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "未知";
+            if (value.ToString() == "0")
+            {
+                return "管理员";
+            }
+            else
+            {
+                return "普通用户";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
