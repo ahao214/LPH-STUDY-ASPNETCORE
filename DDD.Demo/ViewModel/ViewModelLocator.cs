4711413b8734b9d4cb544f0181d54f91a420669d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-06 20:09:31 +0800

    add ViewModelLocator.cs文件

---
+++ DDD.Demo/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.ViewModel
+{
+    public class ViewModelLocator
+    {
+        //public ViewModelLocator()
+        //{
+        //    ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+
+        //    SimpleIoc.Default.Register<MainViewModel>();
+        //}
+
+
+        //public MainViewModel Main
+        //{
+        //    get
+        //    {
+        //        return ServiceLocator.Current.GetInstance<MainViewModel>();
+        //    }
+        //}
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+
+    }
+}
