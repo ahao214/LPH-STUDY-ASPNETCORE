e688538d0c978c489af1b2283d057f00502bc740
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-06 15:04:40 +0800

    C#生成二维码并添加Logo

---
+++ TestQRcodeDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Windows.Media;
+using static System.Console;
+
+namespace TestQRcodeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "C#生成二维码并添加Logo";
+            QRCodeGenerator code = new QRCodeGenerator();
+            QRCodeData codeData = code.CreateQrCode("www.xxx.com", QRCodeGenerator.ECCLevel.H);
+            QRCode qRCode = new QRCode(codeData);
+            Bitmap bitmap = qRCode.GetGraphic(15, System.Drawing.Color.Green, System.Drawing.Color.White, true);
+
+            Image image = Image.FromHbitmap(bitmap.GetHbitmap());
+            image.Save(@"test.png");
+
+            ReadKey();
+        }
+
+    }
+}
