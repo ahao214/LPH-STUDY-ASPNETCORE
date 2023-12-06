532e7db44d1564ca76527664cea7e38183e214e7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-06 15:07:58 +0800

    C#生成二维码并添加Logo

--- TestQRcodeDemo/Program.cs
+++ TestQRcodeDemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using QRCoder;
+using System.Diagnostics;
 using System.Drawing;
+using System.Windows.Media.Imaging;
 using static System.Console;
 
 namespace TestQRcodeDemo
@@ -12,7 +14,12 @@ namespace TestQRcodeDemo
             QRCodeGenerator code = new QRCodeGenerator();
             QRCodeData codeData = code.CreateQrCode("www.xxx.com", QRCodeGenerator.ECCLevel.H);
             QRCode qRCode = new QRCode(codeData);
-            Bitmap bitmap = qRCode.GetGraphic(15, System.Drawing.Color.Green, System.Drawing.Color.White, true);
+            // 不带logo图片
+            //Bitmap bitmap = qRCode.GetGraphic(15, System.Drawing.Color.Green, System.Drawing.Color.White, true);
+
+            // logo图片
+            Bitmap logo = new Bitmap(@"logo.png");
+            Bitmap bitmap = qRCode.GetGraphic(15, System.Drawing.Color.Green, System.Drawing.Color.White, logo, 22, 2, true);
 
             Image image = Image.FromHbitmap(bitmap.GetHbitmap());
             image.Save(@"test.png");
