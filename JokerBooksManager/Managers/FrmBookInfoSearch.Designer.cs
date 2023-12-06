6367d899892583c40ae5010ca6eed45fdf8f6db6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-06 20:55:08 +0800

    add 图书查询窗体

---
+++ JokerBooksManager/Managers/FrmBookInfoSearch.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace JokerBooksManager.Managers
+{
+    partial class FrmBookInfoSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmBookInfoSearch
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1717, 682);
+            this.ControlBoxFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(203)))), ((int)(((byte)(83)))));
+            this.Name = "FrmBookInfoSearch";
+            this.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.Style = Sunny.UI.UIStyle.Green;
+            this.Text = "FrmBookInfoSearch";
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(40)))));
+            this.ZoomScaleRect = new System.Drawing.Rectangle(22, 22, 800, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
