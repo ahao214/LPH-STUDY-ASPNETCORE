506c650337ad980c00c940f98ffa75247c35518f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-07 20:55:01 +0800

    会员类的方法

--- DDD.Demo/Dao/MemberProvider.cs
+++ DDD.Demo/Dao/MemberProvider.cs
@@ -7,31 +7,57 @@ using System.Threading.Tasks;
 
 namespace DDD.Demo.Dao
 {
+    /// <summary>
+    /// 会员类的方法
+    /// </summary>
     public class MemberProvider : IProvider<Member>
     {
         public int Delete(Member t)
         {
-            throw new NotImplementedException();
+            using (BookDBEntities db = new BookDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Deleted;
+                return db.SaveChanges();
+            }
         }
 
         public int Insert(Member t)
         {
-            throw new NotImplementedException();
+            using (BookDBEntities db = new BookDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Added;
+                return db.SaveChanges();
+            }
         }
 
         public List<Member> Select()
         {
-            throw new NotImplementedException();
+            using (BookDBEntities db = new BookDBEntities())
+            {
+                return db.Member.ToList();
+
+            }
         }
 
         public Member Select(int Id)
         {
-            throw new NotImplementedException();
+            using (BookDBEntities db = new BookDBEntities())
+            {
+                var lst = db.Member.Where(item => item.Id == Id).ToList();
+                if (lst.Count == 0)
+                    return new Member();
+                else
+                    return lst[0];
+            }
         }
 
         public int Update(Member t)
         {
-            throw new NotImplementedException();
+            using (BookDBEntities db = new BookDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges();
+            }
         }
     }
 }
