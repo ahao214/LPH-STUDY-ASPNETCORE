fd931ac6577031ceebddb11d3d2037948272d188
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-07 20:59:09 +0800

    模型基础类

---
+++ DDD.Demo/Domain/Model/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.Domain.Model
+{
+    /// <summary>
+    /// 模型基础类
+    /// </summary>
+    public class ModelBase
+    {
+    }
+}
