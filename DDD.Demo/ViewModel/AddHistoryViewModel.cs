7b0f7fc28081b9faa8caf230295406c5c5e68d06
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-07 20:23:37 +0800

    add ViewModel

---
+++ DDD.Demo/ViewModel/AddHistoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.ViewModel
+{
+    /// <summary>
+    /// 添加历史记录ViewModel
+    /// </summary>
+    public class AddHistoryViewModel
+    {
+    }
+}
---
+++ DDD.Demo/ViewModel/BorrowDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.ViewModel
+{
+    /// <summary>
+    /// 借阅详细信息ViewModel
+    /// </summary>
+    public class BorrowDetailViewModel
+    {
+    }
+}
