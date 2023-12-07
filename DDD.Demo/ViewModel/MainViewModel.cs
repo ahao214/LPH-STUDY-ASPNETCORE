d746ee311525847eebf4e195193bc2ebfbd2abec
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-07 20:22:15 +0800

    主页ViewModel

---
+++ DDD.Demo/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.ViewModel
+{
+    /// <summary>
+    /// 主页ViewModel
+    /// </summary>
+    public class MainViewModel
+    {
+    }
+}
