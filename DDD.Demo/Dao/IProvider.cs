4e44683df8fd17ff72f2b02d594b867ead840b74
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-07 20:32:10 +0800

    基础接口

---
+++ DDD.Demo/Dao/IProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.Dao
+{
+    /// <summary>
+    /// 基础接口
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IProvider<T>
+    {
+        List<T> Select();
+        T Select(int Id);
+        int Insert(T t);
+        int Update(T t);
+        int Delete(T t);
+
+    }
+}
