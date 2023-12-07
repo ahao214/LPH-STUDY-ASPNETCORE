5d64c9fe5744fd43d8bd7ca536bd79b2e05ae218
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-07 22:09:52 +0800

    add AppData

---
+++ DDD.Demo/AppData.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDD.Demo.Domain.Model;
+namespace DDD.Demo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AppData:ObservableObject
+    {
+        public MemberModel CurrentUser { get; set; }    
+
+
+    }
+}
