f38ea9c931d10835c922c8963088f6b5c49d4d75
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-08 20:38:03 +0800

    会员接口

--- DDD.Demo/Domain/Interface/IMemberRepository.cs
+++ DDD.Demo/Domain/Interface/IMemberRepository.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace DDD.Demo.Domain.Interface
 {
     /// <summary>
-    /// 会员借口
+    /// 会员接口
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IMemberRepository<T> where T : class MyClass
+    public interface IMemberRepository<T> where T : class
     {
         List<T> Select();
         T Find(int Id);
