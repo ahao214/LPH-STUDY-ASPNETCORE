1f807b7672f8d5150f0abf41a07a32101e68f46b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-08 20:55:44 +0800

    LoginCommand

--- DDD.Demo/ViewModel/LoginViewModel.cs
+++ DDD.Demo/ViewModel/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using DDD.Demo.Dao;
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,24 @@ namespace DDD.Demo.ViewModel
         public AppData AppData { get; set; } = AppData.Instance;
 
 
+        #region 登录事件
 
+        public RelayCommand <Window> LoginCommand
+        {
+            get
+            {
+                return new RelayCommand<Window>((window) =>
+                {
+                    var list = new MemberProvider().Select();
+                    if(list == null || list.Count ==0)
+                    {
+                        MessageBox.Show("当前数据没有用户");
+                    }    
+                });
+            }
+        }
+
+        #endregion
 
         #region 关闭事件
         public RelayCommand<Window> CloseCommand
