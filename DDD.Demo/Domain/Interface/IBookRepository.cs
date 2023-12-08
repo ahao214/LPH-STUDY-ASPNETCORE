dfa6cd73f0b4ef96a3bfebbb20daf48572d86498
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-08 20:33:06 +0800

    图书借口

---
+++ DDD.Demo/Domain/Interface/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Demo.Domain.Interface
+{
+    /// <summary>
+    /// 图书借口
+    /// </summary>
+    public interface IBookRepository<T> where T : class
+    {
+        List<T> Select();
+        T Find(int Id);
+        T Save(T t);
+    }
+}
