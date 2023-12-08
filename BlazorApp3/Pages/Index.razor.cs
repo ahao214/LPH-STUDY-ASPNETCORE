41e104a9a8db199205e94484e648f42cdb73ab3e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-08 14:56:26 +0800

    add 代码

---
+++ BlazorApp3/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+namespace BlazorApp3.Pages
+{
+    public partial class Index
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+        }
+    }
+}
