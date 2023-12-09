171563f00c4f1ef3c9b8a6117307a5f1725e985d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-09 21:10:43 +0800

    add 分页功能

---
+++ JokerBooksManagerBLL/BookBLL/PageSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerBLL.BookBLL
+{
+    /// <summary>
+    /// 分页功能
+    /// </summary>
+    public class PageSet
+    {
+
+
+    }
+}
