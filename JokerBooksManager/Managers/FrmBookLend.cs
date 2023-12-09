27dd8440a846a5a7dd53ed4a1c24b89873ac6273
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-09 22:33:09 +0800

    图书借书窗体

---
+++ JokerBooksManager/Managers/FrmBookLend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+
+
+namespace JokerBooksManager.Managers
+{
+    /// <summary>
+    /// 图书借书窗体
+    /// </summary>
+    public partial class FrmBookLend : UIForm
+    {
+        public FrmBookLend()
+        {
+            InitializeComponent();
+        }
+    }
+}
