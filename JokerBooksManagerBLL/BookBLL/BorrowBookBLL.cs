c5dd5c3bb1f17ea5f48a1706cc19f1a4669fae96
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-10 16:32:14 +0800

    图书借阅

---
+++ JokerBooksManagerBLL/BookBLL/BorrowBookBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerBLL.BookBLL
+{
+    /// <summary>
+    /// 图书借阅
+    /// </summary>
+    public class BorrowBookBLL
+    {
+    }
+}
---
+++ JokerBooksManagerDAL/BookDAL/BorrowBookDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokerBooksManagerDAL.BookDAL
+{
+    /// <summary>
+    /// 图书借阅
+    /// </summary>
+    public class BorrowBookDAL
+    {
+
+
+    }
+}
